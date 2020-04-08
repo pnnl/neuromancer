1b8de831fa7458d2aade874096e17839d2805f2b
Aaron Tuor <aaron.tuor@pnnl.gov>
2020-04-08 08:59:21 -0700

    mostly working now

--- test_all.py
+++ test_all.py
@@ -2,7 +2,7 @@ import os
 
 for ssm in ['vanilla', 'pf', 'svd', 'spectral', 'true']:
     for heatflow in ['black', 'grey', 'white']:
-        for state_estimator in ['true', 'linear', 'pf', 'mlp', 'rnn', 'rnn_const', 'rnn_spectral', 'rnn_svd', 'kf']:
+        for state_estimator in ['true', 'linear', 'pf', 'mlp', 'rnn', 'rnn_constr', 'rnn_spectral', 'rnn_svd', 'kf']:
             print(f'\n###############\n{ssm} {heatflow} {state_estimator}\n#################\n')
             err = os.system(f'python train.py -ssm_type {ssm} -heatflow {heatflow} -state_estimator {state_estimator}')
             if err != 0:

fd6e6250725082142fa74b8fd90a93eea8780cb9
Jan Drgona <jan.drgona@pnnl.gov>
2020-05-03 20:02:30 -0700

    update dispatch

--- dispatch_marianas.py
+++ dispatch_marianas.py
@@ -47,7 +47,8 @@ for path in datapaths:
             for nsteps in [2, 4, 8, 16, 32]:
                 for i in range(args.nsamples): # 10 samples for each configuration
                     cmd = 'python train_loop.py ' +\
-                          '--gpu 0 -epochs 30000 ' +\
+                          '-gpu 0 ' \
+                          '-epochs 30000 ' +\
                           '-location %s ' % args.results + \
                           '-datafile %s' % path + \
                           '-linear_map %s ' % linear + \

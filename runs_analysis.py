596946e894982d1d2de8676d499f3180a36b13ec
Jan Drgona <jan.drgona@pnnl.gov>
2020-05-28 14:58:41 -0700

    update

--- runs_analysis.py
+++ runs_analysis.py
@@ -14,15 +14,30 @@ runs_folder = 'mlruns'
 mlflow.set_tracking_uri(args.expfolder)
 
 runs = mlflow.search_runs(experiment_ids=run_ids, filter_string="",
-                          order_by=["metrics.open_dev_loss ASC"])
+                          order_by=["metrics.bestopen ASC"])
 runs = pd.DataFrame(runs)
 runs.to_csv('nonlin_sysid_2020_5_25.csv')
 runs.to_pickle('nonlin_sysid_2020_5_25.pkl')
-runs = runs[['params.savedir', 'params.datafile', 'metrics.open_dev_reg', 'metrics.open_test_loss',
-             'metrics.open_train_loss', 'metrics.nstep_dev_reg',
-             'metrics.nstep_test_loss', 'metrics.bestdev',
-             'metrics.open_test_reg', 'metrics.train_reg',
-             'metrics.nstep_train_loss', 'metrics.nstep_dev_loss', 'metrics.devloss',
-             'metrics.nstep_test_reg', 'metrics.open_train_reg', 'metrics.trainloss',
-             'metrics.nstep_train_reg', 'metrics.dev_reg', 'metrics.open_dev_loss']]
+runs = runs[['params.savedir', 'params.datafile', 'metrics.trainloss', 'metrics.train_reg',
+             'metrics.devloss', 'metrics.dev_reg', 'metrics.open', 'metrics.bestopen',
+             'metrics.nstep_train_loss', 'metrics.nstep_dev_loss', 'metrics.nstep_test_loss',
+             'metrics.nstep_train_reg', 'metrics.nstep_dev_reg', 'metrics.nstep_test_reg',
+             'metrics.open_train_loss', 'metrics.open_dev_loss', 'metrics.open_test_loss',
+             'metrics.open_train_reg', 'metrics.open_dev_reg', 'metrics.open_test_reg']]
 print(runs)
+
+
+# # # # # #  OLD METRIC SYSTEM # # # # # # # # #
+# runs = mlflow.search_runs(experiment_ids=run_ids, filter_string="",
+#                           order_by=["metrics.open_dev_loss ASC"])
+# runs = pd.DataFrame(runs)
+# runs.to_csv('nonlin_sysid_2020_5_25.csv')
+# runs.to_pickle('nonlin_sysid_2020_5_25.pkl')
+# runs = runs[['params.savedir', 'params.datafile', 'metrics.open_dev_reg', 'metrics.open_test_loss',
+#              'metrics.open_train_loss', 'metrics.nstep_dev_reg',
+#              'metrics.nstep_test_loss', 'metrics.bestdev',
+#              'metrics.open_test_reg', 'metrics.train_reg',
+#              'metrics.nstep_train_loss', 'metrics.nstep_dev_loss', 'metrics.devloss',
+#              'metrics.nstep_test_reg', 'metrics.open_train_reg', 'metrics.trainloss',
+#              'metrics.nstep_train_reg', 'metrics.dev_reg', 'metrics.open_dev_loss']]
+# print(runs)
--- train.py
+++ train.py
@@ -75,7 +75,7 @@ def parse_args():
                         help="Gpu to use")
     # OPTIMIZATION PARAMETERS
     opt_group = parser.add_argument_group('OPTIMIZATION PARAMETERS')
-    opt_group.add_argument('-epochs', type=int, default=5000)
+    opt_group.add_argument('-epochs', type=int, default=1000)
     opt_group.add_argument('-lr', type=float, default=0.003,
                            help='Step size for gradient descent.')
 

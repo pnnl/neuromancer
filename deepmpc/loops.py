e72c20272070fa1e5a331f650c9b7e794453f2b2
Jan Drgona <jan.drgona@pnnl.gov>
2020-07-03 15:47:18 -0700

    strategize over generic model structure via single class

--- deepmpc/loops.py
+++ deepmpc/loops.py
@@ -120,6 +120,177 @@ class ClosedLoop(nn.Module):
         reg_error = reg_error_model + reg_error_policy + reg_error_estim + estim_error
         return Xf, Yf, Uf, reg_error
 
+"""
+CONSTRAINTS DESIGN IDEAS
+
+# TODO: make internal variables from return attributes in models, estimators and policies
+#  such that we can treat them with constraints class
+# e.g., Sxmax = MaxPenalty(model.X, Xmax)
+# Loop input arguments as a list of constraints classes: constr=[]
+# example: constr = [MaxPenalty(model.X, Xmax), MinPenalty(model.X, Xmin),
+#                    MaxPenalty(policy.U, Umax), MinPenalty(policy.U, Umin)]
+# OR symbolic definition with parser:
+# constr = [ X <= Xmax, X >= Xmin, U <= Umax, U >= Umin]
+# TODO: what model binding mechamism we shall use for flexible design?
+# shall we use symbolic variable definition as used in optimization toolboxes?
+# based on used objects we can extract all model variables into a list of object attributes
+"""
+
+
+"""
+OBJECTIVE DESIGN IDEAS
+
+aggregate of torch.nn.functional expressions
+objective = 0
+objective += F.MSE(X-X_trg)
+objective += F.MSE(U)
+objective += f(X)           #  custom loss term
+"""
+
+
+
+class Model(nn.Module):
+    def __init__(self, **linargs):
+        """
+        Base model class of the framework
+        Allows flexible constriction of the loops (Open, Closed), and optimization problems
+
+        Attributes:
+        self.dynamics = system dynamics model
+        self.estim = state estimator
+        self.policy = control policy
+        self.constraints = constraints
+        self.parametric_map = solution map for constrained optimization problem, equivalent to policy for control
+        self.objective = objective function
+        self.variables = all model variables Xi - to be computed by the model
+        self.parameters = all model parameters Theta - to be obtained from the dataset
+        self.type = type of the model: 'OpenLoop', 'ClosedLoop', 'pOP' - parametric optimization problem
+        self.variables - list of all variables of the model, each submodule should have this atribute
+
+        Methods:
+        add() method for stepwise construction of the loop
+        delete() method for dropping unwanted parts of the model
+        compile() method for putting things together
+        forward() for forward pass
+
+        -------------- VARIABLES AND PARAMETERS -------------------
+        variables Xi - computed by the model
+        parameters Theta - obtained from the dataset
+        OpenLoop:
+            variables Xi:
+                states - X, outputs - Y, slacks - S
+            parameters Theta:
+                inputs - U, disturbances - D,
+                min_states - X_min, max_states - X_max,
+                min_outputs - Y_min, max_outputs - Y_max,
+                target_states - X_trg, target_outputs - Y_trg,
+        ClosedLoop:
+            variables Xi:
+                states - X, outputs - Y, inputs - U, slacks - S
+            parameters Theta:
+                disturbances - D,
+                min_states - X_min, max_states - X_max,
+                min_outputs - Y_min, max_outputs - Y_max,
+                min_inputs - U_min, max_inputs - U_max,
+                target_states - X_trg, target_outputs - Y_trg
+        pOP:
+            variables Xi:
+                states - X, slacks - S
+            parameters Theta:
+                min_states - X_min, max_states - X_max, target_states - X_trg,
+
+        --------------- MODEL TYPES ------------------
+        OpenLoop:
+            min objective(Xi,Theta)
+            s.t.
+                X = estim(X,Y,U,D)
+                X, Y = dynamics(X,U,D)
+                S = constraints(Xi,Theta)
+
+        ClosedLoop:
+            min objective(Xi,Theta)
+            s.t.
+                X = estim(X,Y,U,D)
+                U = policy(Xi)
+                X, Y = dynamics(X,U,D)
+                S = constraints(Xi,Theta)
+
+        pOP: parametric optimization problem
+            min objective(X,S,Theta)
+            s.t. S = constraints(X,Theta)
+                 X = parametric_map(Theta)
+
+            more specific optimization problem formulation with hard constraints:
+            https://en.wikipedia.org/wiki/Parametric_programming
+                min_W f(X,Theta)
+                s.t. g_max(X) <= X_max
+                     g_min(X) >= X_min
+                     g_eq(X) = X_trg
+                     X = h_W(Theta)
+                f(X, Theta): objective class
+                g(X, Theta): constraints class
+                h_W(Theta): parametric_map
+
+        """
+        super().__init__()
+        self.dynamics = None
+        self.estim = None
+        self.policy = None
+        self.constraints = []
+        self.parametric_map = None
+        self.objective = None
+        self.variables = None
+        self.parameters = None
+        self.type = None    # 'OpenLoop', 'ClosedLoop', 'pOP'
+
+    def add(self, dynamics=None, estim=None, policy=None, constraints=None,
+                 parametric_map=None, objective=None, **linargs):
+        # add part of the model
+        self.dynamics = dynamics
+        self.estim = estim
+        self.policy = policy
+        self.parametric_map = parametric_map
+        self.objective = objective
+        if constraints is not None:
+            for i in len(constraints):
+                self.constraints.append(constraints[i])
+
+    def delete(self, **linargs):
+        pass
+        # delete part of the model, e.g., policy or particular constraint
+
+    def compile(self):
+        # TODO: step 1, check if comensions of components hold
+        # TODO: setp 2, assign variables and parameters based on the current components
+        # TODO: step 3, define type of the model based on defined components
+        # TODO: step 4, construct compiled model for forward pass based on the current type
+
+        # define type of the model based on its components
+        if self.policy == None and not all((self.dynamics, self.estim, self.objective)):
+            self.type = 'OpenLoop'
+        elif not all((self.dynamics, self.estim, self.policy, self.objective)):
+            self.type = 'ClosedLoop'
+        elif all((self.dynamics, self.estim, self.policy)) \
+                and not all((self.parametric_map, self.objective, self.constraints)):
+            self.type = 'pOP'
+
+        self.variables = None
+        self.parameters = None  # e.g., constraints bounds, references, disturbances - obtained from dataset
+        self.model_compiled = None
+
+        
+    def forward(self, **linargs):
+        # TODO: how to handle varying arguments based on the model type?
+        # define specific forward passes based on the model type? e.g., open loop, closed loop, optimization problem?
+        if self.type == 'OpenLoop':
+            pass
+        elif self.type == 'ClosedLoop':
+            pass
+        elif self.type == 'pOP':
+            pass
+
+
+# TODO: alternative to single model class is definition of constraints and objectives as arguments in loops objects
 
 if __name__ == '__main__':
     nx, ny, nu, nd = 15, 7, 5, 3

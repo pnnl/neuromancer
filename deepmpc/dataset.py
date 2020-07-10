running now but there is an issue with the state estimator. It works for RNN only and is handing out the wrong dimensions for open loop.

deepmpc/dataset.py
@@ -79,7 +79,7 @@ class Dataset:
             vnorm, vmin, vmax = self.normalize(v)
             data[k] = vnorm
             self.min_max_norms.update({k+'min': vmin, k+'max': vmax})
-            self.dims[k], self.dims[k+'p'], self.dims[k+'f'] = v.shape[1]
+            self.dims[k], self.dims[k+'p'], self.dims[k+'f'] = v.shape[1], v.shape[1], v.shape[1]
             loop_data[k+'p'] = torch.tensor(v[:-nsteps], dtype=torch.float32).to(device)
             loop_data[k + 'f'] = torch.tensor(v[nsteps:], dtype=torch.float32).to(device)
             nstep_data[k + 'p'] = batch_data(loop_data[k+'p'], nsteps).to(device)

deepmpc/problem.py
@@ -6,9 +6,6 @@ import torch
 import torch.nn as nn
 
 
-
-
-
 class Objective(nn.Module):
     def __init__(self, variable_names: List[str], loss: Callable[..., torch.Tensor], weight=1.0):
         """
@@ -18,11 +15,12 @@ class Objective(nn.Module):
                                 Arguments to callable should be torch.Tensor and return type a 0-dimensional torch.Tensor
         :param weight: (float) Weight of objective for calculating multi-objective loss function
         """
+        super().__init__()
         self.variable_names = variable_names
         self.weight = weight
         self.loss = loss
 
-    def __call__(self, variables: Dict[str, torch.Tensor]) -> torch.Tensor:
+    def forward(self, variables: Dict[str, torch.Tensor]) -> torch.Tensor:
         """
 
         :param variables: (dict, {str: torch.Tensor}) Should contain keys corresponding to self.variable_names
@@ -61,20 +59,21 @@ class Problem(nn.Module):
         return loss
 
     def forward(self, data: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+
         output_dict = self.step(data)
+        print([(k, v.shape) for k, v in output_dict.items()])
         loss = self._calculate_loss(output_dict)
         output_dict = {'loss': loss, **output_dict}
-        return {f'{data["name"]}_{k}': v for k, v in output_dict.items()}
+        return {f'{data.name}_{k}': v for k, v in output_dict.items()}
 
     def step(self, input_dict: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
-        output_dict = dict()
         for component in self.components:
             output_dict = component(input_dict)
             assert set(output_dict.keys()) - set(input_dict.keys()) == set(output_dict.keys()), \
                 f'Name collision in input and output dictionaries, Input_keys: {input_dict.keys()},' \
                 f'Output_keys: {output_dict.keys()}'
             input_dict = {**input_dict, **output_dict}
-        return output_dict
+        return input_dict
 
 
 

deepmpc/system_id.py
@@ -135,7 +135,7 @@ if __name__ == '__main__':
     nx = dataset.dims['Y']*args.nx_hidden
     nu = dataset.dims['U'] if 'U' in dataset.dims else 0
     nd = dataset.dims['D'] if 'D' in dataset.dims else 0
-    ny = dataset.dime['Y']
+    ny = dataset.dims['Y']
     linmap = linear.maps[args.linear_map]
     nonlinmap = {'linear': linmap,
                  'mlp': blocks.MLP,
@@ -159,11 +159,11 @@ if __name__ == '__main__':
     ##########################################
     ########## MULTI-OBJECTIVE LOSS ##########
     ##########################################
-    estimator_loss = Objective(['X_pred', 'x0_estim'],
-                               lambda X_pred, x0: F.mse_loss(X_pred[-1, :-1, :], x0),
+    estimator_loss = Objective(['X_pred', 'x0'],
+                               lambda X_pred, x0: F.mse_loss(X_pred[-1, :-1, :], x0[1:]),
                                weight=args.Q_e)
     regularization = Objective(['estim_reg_error', 'dynamics_reg_error'], lambda reg1, reg2: reg1 + reg2, weight=args.Q_sub)
-    reference_loss = Objective(['Y_pred, Yf'], F.mse_loss, weight=args.Q_y)
+    reference_loss = Objective(['Y_pred', 'Yf'], F.mse_loss, weight=args.Q_y)
     state_smoothing = Objective(['X_pred'], lambda x: F.mse_loss(x[1:], x[:-1]), weight=args.Q_dx)
     state_lower_bound_penalty = Objective(['X_pred'], lambda x: torch.mean(F.relu(-x + -0.2)), weight=args.Q_con_x)
     state_upper_bound_penalty = Objective(['X_pred'], lambda x: torch.mean(F.relu(x - 1.2)), weight=args.Q_con_x)
@@ -178,5 +178,5 @@ if __name__ == '__main__':
     optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
     visualizer = visuals.NoOpVisualizer()
     trainer = Trainer(model, dataset, optimizer, visualizer=visualizer)
-    best_model = Trainer.train()
+    best_model = trainer.train()
     Trainer.eval(best_model)

deepmpc/trainer.py
@@ -4,7 +4,7 @@ from copy import deepcopy
 import torch
 import numpy as np
 from logger import BasicLogger
-from visuals import Visualizer, NoOpVisualizer
+from visuals import NoOpVisualizer
 from loops import Problem
 from dataset import Dataset
 
@@ -12,8 +12,7 @@ from dataset import Dataset
 class Trainer:
 
     def __init__(self, problem: Problem, dataset: Dataset, optimizer: torch.optim.Optimizer,
-                 logger: BasicLogger = BasicLogger(), visualizer: Visualizer = NoOpVisualizer(), epochs=1000):
-        super().__init__()
+                 logger: BasicLogger = BasicLogger(), visualizer=NoOpVisualizer(), epochs=1000):
         self.model = problem
         self.optimizer = optimizer
         self.dataset = dataset
@@ -33,7 +32,7 @@ class Trainer:
             self.model.train()
             output = self.model(self.dataset.train_data)
             self.optimizer.zero_grad()
-            output['loss'].backward()
+            output['nstep_train_loss'].backward()
             self.optimizer.step()
 
             with torch.no_grad():
@@ -41,9 +40,9 @@ class Trainer:
                 dev_nstep_output = self.model(self.dataset.dev_data)
                 dev_loop_output = self.model(self.dataset.dev_loop)
                 self.logger.log_metrics({**dev_nstep_output, **dev_loop_output}, step=i)
-                if dev_loop_output['loss'] < best_looploss:
+                if dev_loop_output['loop_dev_loss'] < best_looploss:
                     best_model = deepcopy(self.model.state_dict())
-                    best_looploss = dev_loop_output['dev_loop_loss']
+                    best_looploss = dev_loop_output['loop_dev_loss']
                 self.visualizer.plot({**dev_nstep_output, **dev_loop_output}, self.dataset, self.model.state_dict())
 
         plots = self.visualizer.output()
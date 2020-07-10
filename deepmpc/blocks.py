running now but there is an issue with the state estimator. It works for RNN only and is handing out the wrong dimensions for open loop.
Former-commit-id: d3d2fa2d81deac6cd8d4a1aac19684c15d7b2ac7

deepmpc/blocks.py
@@ -67,7 +67,7 @@ class MLP(nn.Module):
 
 class ResMLP(MLP):
     def __init__(self, insize, outsize, bias=True,
-                 Linear=linear.Linear, nonlin=F.gelu, hsizes=[64], skip=1, **linargs):
+                 Linear=linear.Linear, nonlin=F.gelu, hsizes=[64], skip=1, linargs=dict()):
         """
 
         :param insize:
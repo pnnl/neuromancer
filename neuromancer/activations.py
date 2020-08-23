eca62043a6e8ab89b27a94e5af21c31de34d9600
Aaron Tuor <aaron.tuor@pnnl.gov>
2020-08-22 19:25:22 -0700

    Added first pass at neural koopman operator. Added learnable interpolation between addition and multitplication using soft exponential

--- neuromancer/activations.py
+++ neuromancer/activations.py
@@ -3,6 +3,29 @@ import torch
 import torch.nn as nn
 
 
+def soft_exp(alpha, x):
+    if alpha == 0.0:
+        return x
+    elif alpha < 0.0:
+        return -torch.log(1 - alpha * (x + alpha)) / alpha
+    else:
+        return (torch.exp(alpha * x) - 1) / alpha + alpha
+
+
+class InterpolateAddMultiply(nn.Module):
+    """
+    Implementation of smooth interpolation between addition and multiplication
+    using soft exponential activation: https://arxiv.org/pdf/1602.01321.pdf
+    h(β, p, q) = f(β, f(−β, p) + f(−β, q)).
+    """
+    def __init__(self, alpha=0.0):
+        super.__init__()
+        self.alpha = nn.Parameter(torch.tensor(alpha), requires_grad=True)
+
+    def forward(self, p, q):
+        return soft_exp(self.alpha, soft_exp(-self.alpha, p) + soft_exp(-self.alpha, q))
+
+
 class SoftExponential(nn.Module):
     """
     Implementation of soft exponential activation: https://arxiv.org/pdf/1602.01321.pdf
@@ -17,12 +40,7 @@ class SoftExponential(nn.Module):
         self.alpha = nn.Parameter(torch.tensor(alpha), requires_grad=True)
 
     def forward(self, x):
-        if self.alpha == 0.0:
-            return x
-        elif self.alpha < 0.0:
-            return -torch.log(1 - self.alpha * (x + self.alpha)) / self.alpha
-        else:
-            return (torch.exp(self.alpha * x) - 1) / self.alpha + self.alpha
+        return soft_exp(self.alpha, x)
 
 
 class BLU(nn.Module):

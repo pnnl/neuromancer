1835fba5d2691315d9e838ef56b56d5e1c3f4784
Jan Drgona <jan.drgona@pnnl.gov>
2020-09-04 11:02:00 -0700

    update hack dynamics signals generator
    Former-commit-id: 5849ef411ff25046fa094f37107c22238ca818e8

--- neuromancer/signals.py
+++ neuromancer/signals.py
@@ -15,6 +15,25 @@ import psl
 from collections import defaultdict
 import dill
 
+
+
+def freeze_weight(model, module_names=['']):
+    """
+    ['parent->child->child']
+    :param component:
+    :param module_names:
+    :return:
+    """
+    modules = dict(model.named_modules())
+    for name in module_names:
+        freeze_path = name.split('->')
+        if len(freeze_path) == 1:
+            modules[name].requires_grad_(False)
+        else:
+            parent = modules[freeze_path[0]]
+            freeze_weight(parent, ['->'.join(freeze_path[1:])])
+
+
 class SignalGeneratorDynamics(nn.Module):
     def __init__(self, dynamics, estimator, nsteps, xmax=1.0, xmin=0.0, name='signal_dynamics'):
         super().__init__()
@@ -23,6 +42,7 @@ class SignalGeneratorDynamics(nn.Module):
         
         """
         self.nsteps = nsteps
+        self.nbatch = None
         self.nu = dynamics.nu
         self.nd = dynamics.nd
         self.ny = dynamics.ny
@@ -32,37 +52,61 @@ class SignalGeneratorDynamics(nn.Module):
         self.data_dims_in = {k: v[-1] for k, v in estimator.data_dims.items() if k in estimator.input_keys}
         self.xmax, self.xmin, self.name = xmax, xmin, name
         self.estimator, self.dynamics = estimator, dynamics
+        freeze_weight(self.estimator)
+        freeze_weight(self.dynamics)
 
     def forward(self, data):
+        with torch.no_grad():
+            key = list(data.keys())[0]
+            # nsim = data[key].shape[1]*data[key].shape[0] + self.nsteps
+            current_nbatch = data[key].shape[1]
+            if self.nbatch is None:
+                self.nbatch = current_nbatch
+            state_estimator_input = dict()
+            for input_signal, dim in self.data_dims_in.items():
+                numPeriods = random.randint(self.nsteps // 4, self.nsteps // 2)
+                if numPeriods > self.nsteps:
+                    numPeriods = self.nsteps
+                sample = psl.Periodic(nx=dim, nsim=self.nsteps,
+                                      numPeriods=numPeriods,
+                                      xmax=self.xmax, xmin=self.xmin)
+                state_estimator_input[input_signal] = torch.tensor(sample).view(self.nsteps, 1, dim).float()
+            estimator_output = self.estimator(state_estimator_input)
+            ysignals = []
+            dynamics_input = dict()
+            x0 = estimator_output[f'x0_{self.estimator.name}']
+            # TODO: this is a hack!!! FIX in FY21
+            for i in range(self.nbatch+5):
+                for dim, name in zip([self.ny, self.nu, self.nd], self.dynamics_input_keys):
+                    if dim > 0:
+                        dynamics_input[name] = torch.tensor(psl.Periodic(nx=dim, nsim=self.nsteps,
+                                                            numPeriods=random.randint(self.nsteps//4, self.nsteps//2),
+                                                            xmax=self.xmax, xmin=self.xmin)).view(self.nsteps, 1, dim).float()
+                dynamics_input[f'x0_{self.estimator.name}'] = x0
+                dynamics_output = self.dynamics({**estimator_output, **dynamics_input})
+                x0 = dynamics_output[f'X_pred_{self.dynamics.name}'][-1]
+                ysignals.append(dynamics_output[f'Y_pred_{self.dynamics.name}'])
 
-        key = list(data.keys())[0]
-        nsim = data[key].shape[1]*data[key].shape[0] + self.nsteps
-        nbatch = data[key].shape[1]
-        state_estimator_input = dict()
-        for input_signal, dim in self.data_dims_in.items():
-            numPeriods = random.randint(self.nsteps // 4, self.nsteps // 2)
-            if numPeriods > self.nsteps:
-                numPeriods = self.nsteps
-            sample = psl.Periodic(nx=dim, nsim=self.nsteps,
-                                  numPeriods=numPeriods,
-                                  xmax=self.xmax, xmin=self.xmin)
-            state_estimator_input[input_signal] = torch.tensor(sample).view(self.nsteps, 1, dim).float()
-        estimator_output = self.estimator(state_estimator_input)
-        ysignals = []
-        dynamics_input = dict()
-        x0 = estimator_output[f'x0_{self.estimator.name}']
-        for i in range(nbatch+1):
-            for dim, name in zip([self.ny, self.nu, self.nd], self.dynamics_input_keys):
-                if dim > 0:
-                    dynamics_input[name] = torch.tensor(psl.Periodic(nx=dim, nsim=self.nsteps,
-                                                        numPeriods=random.randint(self.nsteps//4, self.nsteps//2),
-                                                        xmax=self.xmax, xmin=self.xmin)).view(self.nsteps, 1, dim).float()
-            dynamics_input[f'x0_{self.estimator.name}'] = x0
-            dynamics_output = self.dynamics({**estimator_output, **dynamics_input})
-            x0 = dynamics_output[f'X_pred_{self.dynamics.name}'][-1]
-            ysignals.append(dynamics_output[f'Y_pred_{self.dynamics.name}'])
-        Yp = torch.stack(ysignals[:-1], dim=1).view(self.nsteps, nbatch, self.ny)
-        Yf = torch.stack(ysignals[1:], dim=1).view(self.nsteps, nbatch, self.ny)
+            if self.nbatch == current_nbatch:
+                # TODO: this is a hack!!! FIX in FY21
+                Yp = torch.stack(ysignals[:-5], dim=1).view(self.nsteps, self.nbatch, self.ny)
+                Yf = torch.stack(ysignals[1:-4], dim=1).view(self.nsteps, self.nbatch, self.ny)
+            else:
+                # TODO: this is a hack!!! FIX in FY21
+                # print(ysignals[0].shape)
+                # print(len(ysignals))
+                Yp = torch.cat(ysignals[:-1]).squeeze(1)
+                Yf = torch.cat(ysignals[1:]).squeeze(1)
+                # print(Yp.shape)
+                # print(Yf.shape)
+                end_step_Yp = Yp.shape[0] - self.nsteps
+                end_step_Yf = Yf.shape[0] - self.nsteps
+                # print(end_step_Yp)
+                # print(end_step_Yf)
+                Yp = torch.stack([Yp[k:k + self.nsteps, :] for k in range(0, end_step_Yp)]).transpose(1, 0)  # nsteps X nsamples X nfeatures
+                Yf = torch.stack([Yf[k:k + self.nsteps, :] for k in range(0, end_step_Yf)]).transpose(1, 0)  # nsteps X nsamples X nfeatures
+                Yp = Yp[:, :2993, :]
+                Yf = Yf[:, :2993, :]
         return {self.name + 'p': Yp, self.name + 'f': Yf}
 
 

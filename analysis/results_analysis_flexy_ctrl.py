7af4841b5e6e4a6eeae42ebf642af31f379de1c9
Jan Drgona <jan.drgona@pnnl.gov>
2020-09-08 19:44:07 -0700

    update visuals closed loop
    Former-commit-id: 506fc3f48fa53a1db83be48f4cbe45ebb3bd0cd0

--- analysis/results_analysis_flexy_ctrl.py
+++ analysis/results_analysis_flexy_ctrl.py
@@ -13,63 +13,67 @@ import os
 
 
 
-datapaths = ['./datasets/EED_building/EED_building.csv']
-systems = ['building_EEd']
-models = ['blocknlin', 'blackbox']
+systems = ['flexy air ctrl']
+
+
 linear_map = ['linear', 'pf', 'softSVD']
-nonlinear_map = ['mlp', 'residual_mlp', 'rnn']
-N = ['1','8', '16', '32', '64']
-bias_onoff = ['-bias', '']
-constrainted = ['unconstr', 'constr']
+N = [4, 8, 16]
+policy = ['mlp', 'rnn']
+activations = ['gelu', 'softexp']
+models = ['/people/drgo694/neuromancer/neuromancer/neuromancer/datasets/Flexy_air/best_model_flexy1.pth',
+          '/qfs/projects/deepmpc/best_flexy_models/best_blocknlin_nlinsearch/best_model.pth',
+          '/people/drgo694/neuromancer/neuromancer/neuromancer/datasets/Flexy_air/best_model_flexy2.pth']
 
 
 def process_res(res, key, system_metrics={}):
     # system_metrics[key] = {}
     if not res.empty:
-        if res['metrics.best_loop_test_loss'].idxmin() is not np.nan:
-            best = res.loc[res['metrics.best_loop_test_loss'].idxmin()]
+        if res['metrics.best_nstep_test_ref_loss'].idxmin() is not np.nan:
+            best = res.loc[res['metrics.best_nstep_test_ref_loss'].idxmin()]
         else:
             best = None
         system_metrics[key]['best'] = best
-        res = res.loc[res['metrics.best_loop_dev_loss'].notnull()]
+        res = res.loc[res['metrics.best_nstep_dev_ref_loss'].notnull()]
         # extract metrics
-        devnsteploss = res['metrics.best_nstep_dev_loss']
-        devnsteploss = res['metrics.best_nstep_dev_loss']
-        devopenloss = res['metrics.best_loop_dev_loss']
-        testnsteploss = res['metrics.best_nstep_test_loss']
-        testopenloss = res['metrics.best_loop_test_loss']
-        trainnsteploss = res['metrics.best_nstep_train_loss']
-        trainopenloss = res['metrics.best_loop_train_loss']
+        nstep_dev = res['metrics.best_nstep_dev_loss']
+        nstep_test = res['metrics.best_nstep_test_loss']
+        nstep_test_ref = res['metrics.best_nstep_test_ref_loss']
+        nstep_dev_ref = res['metrics.best_nstep_dev_ref_loss']
+
         # log metrics
-        system_metrics[key]['mean_dev_openloss'] = devopenloss.mean()
-        system_metrics[key]['mean_dev_nsteploss'] = devnsteploss.mean()
-        system_metrics[key]['mean_test_openloss'] = testopenloss.mean()
-        system_metrics[key]['mean_test_nsteploss'] = testnsteploss.mean()
-        system_metrics[key]['std_dev_openloss'] = devopenloss.std()
-        system_metrics[key]['std_dev_nsteploss'] = devnsteploss.std()
-        system_metrics[key]['std_test_openloss'] = testopenloss.std()
-        system_metrics[key]['std_test_nsteploss'] = testnsteploss.std()
-        system_metrics[key]['min_dev_openloss'] = devopenloss.min()
-        system_metrics[key]['min_dev_nsteploss'] = devnsteploss.min()
-        system_metrics[key]['min_test_openloss'] = testopenloss.min()
-        system_metrics[key]['min_test_nsteploss'] = testnsteploss.min()
+        system_metrics[key]['mean_nstep_dev'] = nstep_dev.mean()
+        system_metrics[key]['mean_nstep_test'] = nstep_test.mean()
+        system_metrics[key]['mean_nstep_test_ref'] = nstep_test_ref.mean()
+        system_metrics[key]['mean_nstep_dev_ref'] = nstep_dev_ref.mean()
+
+        system_metrics[key]['std_nstep_dev'] = nstep_dev.std()
+        system_metrics[key]['std_nstep_test'] = nstep_test.std()
+        system_metrics[key]['std_nstep_test_ref'] = nstep_test_ref.std()
+        system_metrics[key]['std_nstep_dev_ref'] = nstep_dev_ref.std()
+
+        system_metrics[key]['min_nstep_dev'] = nstep_dev.min()
+        system_metrics[key]['min_nstep_test'] = nstep_test.min()
+        system_metrics[key]['min_nstep_test_ref'] = nstep_test_ref.min()
+        system_metrics[key]['min_nstep_dev_ref'] = nstep_dev_ref.min()
         return system_metrics
 
 if __name__ == '__main__':
 
-    res = pandas.read_pickle("../results_files/eed_sysid_2020_5_31.pkl")
-    res.rename(columns={'params.ssm_type': 'models',
-                        'params.linear_map': 'linear_map', 'params.nonlinear_map': 'nonlinear_map',
+    # res = pandas.read_pickle("../results/flexy_deepmpc_2020_9_8.pkl")
+    res = pandas.read_csv("../results/flexy_deepmpc_2020_9_8.csv")
+
+    res.rename(columns={'params.model_file': 'models',  'params.linear_map': 'linear_map',
+                        'params.activation': 'activation', 'params.policy': 'policy',
                         'params.nsteps': 'nsteps'}, inplace=True)
     # hierarchical index
     res.reset_index(inplace=True)
-    res.set_index(['index', 'models', 'linear_map', 'nonlinear_map', 'nsteps'], drop=False, inplace=True)
+    res.set_index(['index', 'models', 'linear_map', 'activation', 'policy', 'nsteps'], drop=False, inplace=True)
     res.index
 
     system_metrics = {}
     if not res.empty:
-        if res['metrics.best_loop_test_loss'].idxmin() is not np.nan:
-            best = res.loc[res['metrics.best_loop_test_loss'].idxmin()]
+        if res['metrics.best_nstep_dev_ref_loss'].idxmin() is not np.nan:
+            best = res.loc[res['metrics.best_nstep_dev_ref_loss'].idxmin()]
         else:
             best = None
         system_metrics['best'] = best
@@ -85,9 +89,9 @@ if __name__ == '__main__':
                 system_metrics[nstep][type][linear] = {}
                 res_system_N_type_lin = res_system_N_type.loc[res_system_N_type.linear_map == linear]
                 system_metrics[nstep][type] = process_res(res=res_system_N_type_lin, key=linear, system_metrics=system_metrics[nstep][type])
-                for nonlinear in nonlinear_map:
+                for nonlinear in policy:
                     system_metrics[nstep][type][linear][nonlinear] = {}
-                    res_system_N_type_nonlin = res_system_N_type_lin.loc[res_system_N_type_lin.nonlinear_map == nonlinear]
+                    res_system_N_type_nonlin = res_system_N_type_lin.loc[res_system_N_type_lin.policy == nonlinear]
                     system_metrics[nstep][type][linear] = process_res(res=res_system_N_type_nonlin, key=nonlinear,
                                                  system_metrics=system_metrics[nstep][type][linear])
 
@@ -98,38 +102,34 @@ if __name__ == '__main__':
     # # # # # # # # # #
     idx = []
     for model in models:
-        for linear in linear_map:
-            for nonlinear in nonlinear_map:
-                idx.append(model+'_'+linear+'_'+nonlinear)
-    stdopen, stdnstep, meanopen, meannstep, minopen, minnstep = \
+        idx.append(model)
+    stdopen, stdnstep, meanopen, meannstep, min_nstep_dev, min_nstep_test = \
         [pandas.DataFrame(index=idx,
           columns=N) for i in range(6)]
 
     for i in N:
         for model in models:
-            for linear in linear_map:
-                for nonlinear in nonlinear_map:
-                    # if not not metrics_df[i]:
-                    #     if not not metrics_df[i][model][linear][nonlinear]:
-                    stdopen.loc[model+'_'+linear+'_'+nonlinear,i] = \
-                        metrics_df[i][model][linear][nonlinear]['std_test_openloss']
-                    stdnstep.loc[model+'_'+linear+'_'+nonlinear,i] = \
-                        metrics_df[i][model][linear][nonlinear]['std_test_nsteploss']
-                    meanopen.loc[model+'_' + linear + '_' + nonlinear, i] = \
-                        metrics_df[i][model][linear][nonlinear]['mean_test_openloss']
-                    meannstep.loc[model+'_' + linear + '_' + nonlinear, i] = \
-                        metrics_df[i][model][linear][nonlinear]['mean_test_nsteploss']
-                    minopen.loc[model+'_' + linear + '_' + nonlinear, i] = \
-                        metrics_df[i][model][linear][nonlinear]['min_test_openloss']
-                    minnstep.loc[model+'_' + linear + '_' + nonlinear, i] = \
-                        metrics_df[i][model][linear][nonlinear]['min_test_nsteploss']
+            # if not not metrics_df[i]:
+            #     if not not metrics_df[i][model][linear][nonlinear]:
+            stdopen.loc[model,i] = \
+                metrics_df[i][model]['std_nstep_dev_ref']
+            stdnstep.loc[model,i] = \
+                metrics_df[i][model]['std_nstep_test_ref']
+            meanopen.loc[model, i] = \
+                metrics_df[i][model]['mean_nstep_dev_ref']
+            meannstep.loc[model, i] = \
+                metrics_df[i][model]['mean_nstep_test_ref']
+            min_nstep_dev.loc[model, i] = \
+                metrics_df[i][model]['min_nstep_dev_ref']
+            min_nstep_test.loc[model, i] = \
+                metrics_df[i][model]['min_nstep_test_ref']
 
     # # # # # # # # # #
     # PLOTS and Tables
     # # # # # # # # # #
 
     # Latex Table
-    for k in [stdopen, stdnstep, meanopen, meannstep, minopen, minnstep]:
+    for k in [stdopen, stdnstep, meanopen, meannstep, min_nstep_dev, min_nstep_test]:
         print(k.to_latex(float_format=lambda x: '%.3f' % x))
 
     # Bar plot
@@ -138,34 +138,33 @@ if __name__ == '__main__':
     ind = np.arange(len(N))
     for i, n in enumerate(idx):
         # plt.bar(ind + i * width, minopen.iloc[i], width, label=n, edgecolor='white')
-        plt.bar(ind+i*width, minopen.loc[n], width, label=n, edgecolor='white')
+        plt.bar(ind+i*width, min_nstep_dev.loc[n], width, label=n, edgecolor='white')
     plt.xlabel('Training Prediction Horizon')
-    plt.ylabel('Open loop MSE')
+    plt.ylabel('min nstep dev MSE')
     plt.xticks(ind + width, N)
     plt.legend(loc='best')
     plt.tight_layout()
     plt.grid()
     plt.yscale("log")
-    plt.savefig('../figs/open_loop_'+model+'.eps')
-    plt.savefig('../figs/open_loop_'+model+'.png')
+    # plt.savefig('../figs/open_loop.eps')
+    # plt.savefig('../figs/open_loop_.png')
 
 
     fig = plt.figure(figsize=(14, 10))
     ind = np.arange(len(N))
     for i, n in enumerate(idx):
-        plt.bar(ind+i*width, minnstep.loc[n], width, label=n, edgecolor='white')
+        plt.bar(ind+i*width, min_nstep_test.loc[n], width, label=n, edgecolor='white')
     plt.xlabel('Training Prediction Horizon')
-    plt.ylabel('N-step MSE')
+    plt.ylabel('min nstep test MSE')
     plt.xticks(ind + 1.5*width,  N)
     plt.legend(loc='best')
     plt.tight_layout()
     plt.grid()
     plt.yscale("log")
-    plt.savefig('../figs/nstep_mse_'+model+'.eps')
-    plt.savefig('../figs/nstep_mse_'+model+'.png')
+    # plt.savefig('../figs/nstep_mse.eps')
+    # plt.savefig('../figs/nstep_mse.png')
 
     # copy artifacts
     if False:
-        os.system('mkdir ../results_files/'+system_metrics['best']['params.savedir'])
-        os.system('scp drgo694@ohmahgerd:'+system_metrics['best']['artifact_uri']+'/* '
-                  +'../results_files/'+system_metrics['best']['params.savedir'])
+        os.system('mkdir ..\\results\\'+system_metrics['best']['params.savedir'].split('.')[0].split('/')[1])
+        os.system('scp drgo694@ohmahgerd:'+system_metrics['best']['artifact_uri']+'/* '+'..\\results\\'+system_metrics['best']['params.savedir'].split('.')[0].split('/')[1])

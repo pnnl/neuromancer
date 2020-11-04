aff39824622c962edf503508d05a0e6efa3e8523
Jan Drgona <jan.drgona@pnnl.gov>
2020-11-04 15:48:58 -0800

    update L4DC plots
    Former-commit-id: df24070b5877dac59a19bb324a91a81a5f180e1f

--- neuromancer/train_scripts/L4DC_paper/L4DC_plots.py
+++ neuromancer/train_scripts/L4DC_paper/L4DC_plots.py
@@ -42,14 +42,27 @@ if __name__ == '__main__':
         activations.append(f)
 
     X_stable = []
+    X_BIBO_stable = []
     X_unstable = []
     Act_stable = []
+    Act_BIBO_stable = []
     Act_unstable = []
     for act in activations:
         x = act(x_line).detach().numpy()
         if any(abs(x/x_vec) > 1):
-            X_unstable.append(x)
-            Act_unstable.append(act)
+            # x_1 = act(torch.tensor([5.0])).detach().numpy()
+            # x_2 = act(torch.tensor([6.0])).detach().numpy()
+            # x_3 = act(torch.tensor([-5.0])).detach().numpy()
+            # x_4 = act(torch.tensor([-6.0])).detach().numpy()
+            # if abs(x_1 - x_2).squeeze() <= 0.01 and abs(x_3 - x_4).squeeze() <= 0.01:
+            x_1 = act(torch.tensor([-6.0])).detach().numpy()
+            x_2 = act(torch.tensor([6.0])).detach().numpy()
+            if abs(x_1).squeeze() <= 6.0 and abs(x_2).squeeze() <= 6.0:
+                X_BIBO_stable.append(x)
+                Act_BIBO_stable.append(act)
+            else:
+                X_unstable.append(x)
+                Act_unstable.append(act)
         else:
             X_stable.append(x)
             Act_stable.append(act)
@@ -58,7 +71,7 @@ if __name__ == '__main__':
     print(f'stable activations: {Act_stable}')
 
     # Stable activations
-    rgb_colors = get_colors(len(X_stable))
+    rgb_colors = get_colors(len(X_stable)+1)
     plt.style.use('classic')
     fig, (axe) = plt.subplots(1, 1)
     axe.set_ylim(-6, 6)
@@ -73,7 +86,7 @@ if __name__ == '__main__':
     plt.tight_layout()
 
     # Unstable activations
-    rgb_colors = get_colors(len(X_unstable))
+    rgb_colors = get_colors(len(X_unstable)+1)
     plt.style.use('classic')
     fig, (axe) = plt.subplots(1, 1)
     axe.set_ylim(-6, 6)
@@ -87,6 +100,20 @@ if __name__ == '__main__':
     axe.grid(True)
     plt.tight_layout()
 
+    # BIBO stable activations
+    rgb_colors = get_colors(len(X_BIBO_stable)+1)
+    plt.style.use('classic')
+    fig, (axe) = plt.subplots(1, 1)
+    axe.set_ylim(-6, 6)
+    axe.set_xlim(-6, 6)
+    axe.set_aspect(1)
+    axe.fill_between(x_vec, x_vec, -x_vec, alpha=0.1)
+    for k in range(len(X_BIBO_stable)):
+        axe.plot(x_vec, X_BIBO_stable[k], c=rgb_colors[k], linewidth=2)
+    axe.tick_params(axis='x', labelsize=18)
+    axe.tick_params(axis='y', labelsize=18)
+    axe.grid(True)
+    plt.tight_layout()
 
 
 

4a5576c3b0941f72c95c8cba01bc5a345840d776
Elliott Skomski <elliott.skomski@pnnl.gov>
2020-11-17 09:20:17 -0800

    updates to vis code
    Former-commit-id: 4161f4a1ecc462fcf9d472dbb55bfb968aaab87e

--- neuromancer/train_scripts/L4DC_paper/eigen_plots.py
+++ neuromancer/train_scripts/L4DC_paper/eigen_plots.py
@@ -60,17 +60,19 @@ def plot_eigenvalues(eigvals, ax=None, fname=None):
         lw=2,
     )
     ax.add_patch(patch)
+    """
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         sns.kdeplot(
             x=eigvals.real,
             y=eigvals.imag,
             fill=True,
-            levels=100,
+            levels=50,
             thresh=0,
             cmap=DENSITY_PALETTE,
             ax=ax,
         )
+    """
     sns.scatterplot(
         x=eigvals.real, y=eigvals.imag, alpha=0.5, ax=ax, color="white", s=7
     )
@@ -78,6 +80,7 @@ def plot_eigenvalues(eigvals, ax=None, fname=None):
 
     if fname is not None:
         plt.savefig(fname)
+        plt.close()
 
     return [ax]
 
@@ -98,23 +101,24 @@ def plot_matrix_and_eigvals(item, ax=None):
     return [*ax]
 
 
-def plot_Astar_anim(Astar_list, eigval_list, fname=None):
-    items = zip(Astar_list, eigval_list)
+def plot_matrix_eigval_anim(mat_list, eigval_list, fname=None):
+    items = zip(mat_list, eigval_list)
     fig, ax = plt.subplots(1, 2)
     animator = FuncAnimation(
         fig,
         plot_matrix_and_eigvals,
         items,
         fargs=(ax,),
-        interval=128,
+        interval=16.66666,
         repeat_delay=3000,
         blit=False,
     )
     writer = writers["ffmpeg"](
-        fps=20, bitrate=512000, codec="h264_mf", extra_args=["-s", "1280x960"]
+        fps=60, bitrate=512000, codec="h264_mf", extra_args=["-s", "1280x960"]
     )
     if fname is not None:
         animator.save(fname, writer=writer, dpi=200)
+        plt.close()
 
 
 def gen_model_visuals(
@@ -134,13 +138,13 @@ def gen_model_visuals(
     eigvals = []
     x = estim(loop_data)["x0_estim"]
     for u in loop_data["Up"]:
-        A_star = lpv(fx, x)
+        _, A_star_b, _, _, _, _, _ = lpv(fx, x)
         x = fx(x) + fu(u)  # torch.matmul(x, A_star) + fu(u)
-        A_stars += [A_star.detach().cpu().numpy()]
-        eigvals += [w_net]
+        A_stars += [A_star_b.detach().cpu().numpy()]
 
+    eigvals = compute_eigenvalues(A_stars)
     plot_eigenvalues(eigvals, fname=eigval_plot_fname)
-    plot_Astar_anim(A_stars, eigvals, fname=anim_fname)
+    plot_matrix_eigval_anim(A_stars, eigvals, fname=anim_fname)
 
 
 def despine_axis(ax):
@@ -153,7 +157,55 @@ def despine_axis(ax):
 
 
 if __name__ == "__main__":
-    linmap = slim.linear.maps["gershgorin"]
+    nx = 2
+    square_maps = [
+        ("gershgorin", -1.5, -1.1),
+        ("gershgorin", 0.0, 1.0),
+        ("gershgorin", 0.99, 1.1),
+        ("gershgorin", 1.1, 1.5),
+        # ("pf", 1.0, 1.0),
+        # ("linear", 1.0, 1.0),
+    ]
+
+    activations = [
+        ("relu", torch.nn.ReLU),
+        ("selu", torch.nn.SELU),
+        ("tanh", torch.nn.Tanh),
+        ("sigmoid", torch.nn.Sigmoid),
+    ]
+
+    for nlayers in [1, 8]:
+        for linmap, sigmin, sigmax in square_maps:
+            for real in [True, False]:
+                for bias in [True, False]:
+                    for act_name, act in activations:
+                        fx = blocks.MLP(
+                            nx,
+                            nx,
+                            nonlin=act,
+                            Linear=slim.linear.maps[linmap],
+                            hsizes=[nx] * nlayers,
+                            bias=bias,
+                            linargs={
+                                "sigma_min": sigmin,
+                                "sigma_max": sigmax,
+                                "real": real,
+                            },
+                        )
+
+                        Astars = []
+                        grid_x, grid_y = torch.meshgrid(
+                            torch.arange(-6, 6, 0.5),
+                            torch.arange(-6, 6, 0.5),
+                        )
+                        X = torch.stack((grid_x.flatten(), grid_y.flatten())).T
+                        for x in X:
+                            Astar, Astar_b, _, _, _, _, _ = lpv(fx, x)
+                            Astars += [Astar_b.detach().cpu().numpy()]
+                        eigvals = compute_eigenvalues(Astars)
+                        plot_eigenvalues(eigvals, fname=f"spectrum_{linmap}_({sigmin},{sigmax})_{act_name}_{nlayers}l_{'real' if real else 'complex'}{'_bias' if bias else ''}.png")
+                        plot_matrix_eigval_anim(Astars, eigvals, fname=f"spectrum_{linmap}_({sigmin},{sigmax})_{act_name}_{nlayers}l_{'real' if real else 'complex'}{'_bias' if bias else ''}.mp4")
+
 
     # NOTE: this prints a lot of stuff; use `lpv` for less printing
     # tut_system = AutonomousSystem(2, [2] * 4, nn.ReLU, linmap, 0.9, 1.0)
@@ -167,22 +219,6 @@ if __name__ == "__main__":
         hsizes=[2] * 4,
         linargs=dict(sigma_min=0.9, sigma_max=1.0, real=True),
     )
-
-    Astars = []
-    grid_x, grid_y = torch.meshgrid(
-        torch.arange(-1, 1, 0.05),
-        torch.arange(-1, 1, 0.05),
-    )
-    X = torch.stack((grid_x.flatten(), grid_y.flatten())).T
-    for x in X:
-        Astar, _, _ = lpv(fx, x)
-        Astars += [Astar.detach().cpu().numpy()]
-    eigvals = compute_eigenvalues(Astars)
-    plot_eigenvalues(eigvals)
-    plt.show()
-
-    plot_Astar_anim(Astars, eigvals, "gershgorin.mp4")
-
     exit()
 
     # TODO: dead code follows
@@ -199,7 +235,7 @@ if __name__ == "__main__":
     eigvals = compute_eigenvalues(random_matrices)
 
     plot_eigenvalues(eigvals, fname="random_matrices.svg")
-    plot_Astar_anim(random_matrices, eigvals, fname="random_matrices.mp4")
+    plot_matrix_eigval_anim(random_matrices, eigvals, fname="random_matrices.mp4")
 
     print("Testing visualizations on model...")
     gen_model_visuals(
--- neuromancer/train_scripts/L4DC_paper/phase_plots.py
+++ neuromancer/train_scripts/L4DC_paper/phase_plots.py
@@ -7,6 +7,13 @@ from neuromancer import blocks
 from neuromancer.datasets import EmulatorDataset
 from psl.nonautonomous import TwoTank, CSTR
 from sklearn.decomposition import PCA
+import seaborn as sns
+
+PALETTE = sns.color_palette(
+    "light:#00f", as_cmap=True
+)  # sns.color_palette("crest_r", as_cmap=True)
+
+from lpv import lpv
 
 
 def plot_ode_phase_portrait(
@@ -74,6 +81,7 @@ def plot_model_phase_portrait(
     data=None,
     initial_states=[],
     t=100,
+    regions=True,
     fname=None,
 ):
     """
@@ -85,6 +93,34 @@ def plot_model_phase_portrait(
     if `initial_states` is specified, trajectories over `t` steps will
     be plotted over portrait.
     """
+    _, ax = plt.subplots()
+    if regions:
+        X, Y = torch.meshgrid(
+            torch.arange(*x_lims, (x_lims[1] - x_lims[0]) / 1024),
+            torch.arange(*y_lims, (y_lims[1] - y_lims[0]) / 1024),
+        )
+        grid = torch.stack((X.flatten(), Y.flatten())).T
+        grid = model(grid)
+
+        U, V = grid.T.reshape(2, *X.shape).detach().cpu().numpy()
+
+        U = -X + U
+        V = -Y + V
+
+        magnitudes = np.stack((U, V), axis=-1)
+        magnitudes = np.linalg.norm(magnitudes, ord=2, axis=-1).T[::-1, :]
+        ax.imshow(
+            magnitudes,
+            extent=[
+                x_lims[0] - step / 2,
+                x_lims[1] - step / 2,
+                y_lims[0] - step / 2,
+                y_lims[1] - step / 2,
+            ],
+            interpolation="bicubic",
+            cmap=PALETTE,
+        )
+
     X, Y = torch.meshgrid(torch.arange(*x_lims, step), torch.arange(*y_lims, step))
     grid = torch.stack((X.flatten(), Y.flatten())).T
     grid = model(grid)
@@ -95,7 +131,6 @@ def plot_model_phase_portrait(
     V = -Y + V
 
     # plot vector field
-    _, ax = plt.subplots()
     ax.quiver(
         X, Y, U, V, angles="uv", pivot="mid", width=0.002, headwidth=4, headlength=5
     )
@@ -112,13 +147,127 @@ def plot_model_phase_portrait(
         for i in range(t):
             states[i + 1, :, :] = model(states[i, :, :])
         states = states.transpose(1, 2).detach().cpu().numpy()
-        ax.plot(states[:, 0], states[:, 1])
+        ax.plot(states[:, 0], states[:, 1], marker="o", ms=3)
 
     ax.set_xlim((x_lims[0] - step / 2, x_lims[1] - step / 2))
     ax.set_ylim((y_lims[0] - step / 2, y_lims[1] - step / 2))
 
     if fname is not None:
         plt.savefig(fname)
+        plt.close()
+
+    return [ax]
+
+
+def plot_astar_phase_portrait(
+    model,
+    x_lims=(0, 1),
+    y_lims=(0, 1),
+    step=0.05,
+    use_bias=True,
+    data=None,
+    initial_states=[],
+    t=20,
+    regions=True,
+    fname=None,
+):
+    """
+    generate phase portrait using a model function.
+
+    accepts a function will be evaluated for one step using a grid of initial
+    conditions determined by `x_lims`, `y_lims`, and `step`.
+
+    if `initial_states` is specified, trajectories over `t` steps will
+    be plotted over portrait.
+    """
+    X, Y = torch.meshgrid(
+        torch.arange(x_lims[0] - step, x_lims[1] + step, step),
+        torch.arange(y_lims[0] - step, y_lims[1] + step, step),
+    )
+    grid = torch.stack((X.flatten(), Y.flatten())).T
+
+    Xplus = torch.empty_like(grid)
+    for i, sample in enumerate(grid):
+        Astar, Astar_b, bstar, _, _, _, _ = lpv(model, sample)
+        if use_bias:
+            Xplus[i, ...] = torch.matmul(sample, Astar_b) + bstar
+        else:
+            Xplus[i, ...] = torch.matmul(sample, Astar)
+
+    U, V = Xplus.T.reshape(2, *X.shape).detach().cpu().numpy()
+
+    U = -X + U
+    V = -Y + V
+
+    _, ax = plt.subplots()
+    if regions:
+        magnitudes = np.stack((U, V), axis=-1)
+        magnitudes = np.linalg.norm(magnitudes, ord=2, axis=-1).T[::-1, :]
+        ax.imshow(
+            magnitudes,
+            extent=[
+                x_lims[0] - step / 2,
+                x_lims[1] - step / 2,
+                y_lims[0] - step / 2,
+                y_lims[1] - step / 2,
+            ],
+            interpolation="bicubic",
+            cmap=PALETTE,
+        )
+
+    """
+    X, Y = torch.meshgrid(
+        torch.arange(x_lims[0], x_lims[1] + step, step),
+        torch.arange(y_lims[0], y_lims[1] + step, step),
+    )
+    grid = torch.stack((X.flatten(), Y.flatten())).T
+
+    Xplus = torch.empty_like(grid)
+    for i, sample in enumerate(grid):
+        Astar, Astar_b, bstar, _, _, _, _ = lpv(model, sample)
+        if use_bias:
+            Xplus[i, ...] = torch.matmul(sample, Astar_b) + bstar
+        else:
+            Xplus[i, ...] = torch.matmul(sample, Astar)
+
+    U, V = Xplus.T.reshape(2, *X.shape).detach().cpu().numpy()
+
+    U = -X + U
+    V = -Y + V
+    """
+
+    # plot vector field
+    ax.quiver(
+        X, Y, U, V, angles="uv", pivot="mid", width=0.002, headwidth=4, headlength=5
+    )
+
+    # plot data over phase portrait
+    if data is not None:
+        data_states = data["Yp"].squeeze(1).cpu().numpy()
+        ax.scatter(data_states[:, 0], data_states[:, 1], s=2, alpha=0.7, c="red")
+
+    # plot state trajectories over phase space if initial states given
+    if len(initial_states) > 0:
+        states = torch.empty(t + 1, *initial_states.shape, requires_grad=False)
+        states[0, :, :] = initial_states
+        for i in range(t):
+            for k in range(initial_states.shape[0]):
+                sample = states[i, k, :]
+                Astar, Astar_b, bstar, _, _, _, _ = lpv(model, sample)
+                if use_bias:
+                    states[i + 1, k, :] = torch.matmul(sample, Astar_b) + bstar
+                else:
+                    states[i + 1, k, :] = torch.matmul(sample, Astar)
+        states = states.transpose(1, 2).detach().cpu().numpy()
+        ax.plot(states[:, 0], states[:, 1], marker="o", ms=3)
+
+    ax.set_xlim((x_lims[0] - step / 2, x_lims[1] - step / 2))
+    ax.set_ylim((y_lims[0] - step / 2, y_lims[1] - step / 2))
+    ax.set_aspect(1)
+
+    if fname is not None:
+        plt.savefig(fname)
+        plt.close()
 
     return [ax]
 
@@ -180,11 +329,15 @@ if __name__ == "__main__":
     tank_model = torch.load(TANK_PATH, pickle_module=dill, map_location="cpu")
     tank_data = EmulatorDataset("TwoTank", nsim=10000, seed=81, device="cpu")
 
-    initial_states = [
-        np.array([0.5, 0.25]),
-        np.array([0.5, 0.5]),
-        np.array([0.5, 0.75]),
-    ]
+    initial_states = torch.from_numpy(
+        np.array(
+            [
+                [0.5, 0.25],
+                [0.5, 0.5],
+                [0.5, 0.75],
+            ]
+        )
+    )
 
     print("Testing model phase portrait...")
 

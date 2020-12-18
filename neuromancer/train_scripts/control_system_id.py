b657df584eafb7a5b4740278f4206a4d23ffe644
Elliott Skomski <elliott.skomski@pnnl.gov>
2020-12-17 16:06:30 -0800

    "fixed" argument conflicts in control script
    Former-commit-id: 982ebfe4a2e47001ed11b36a96ceac1155f78bed

--- neuromancer/train_scripts/control_system_id.py
+++ neuromancer/train_scripts/control_system_id.py
@@ -1,6 +1,7 @@
 """
 Script for training neural control policies based on system identification.
 """
+import argparse
 
 import torch
 from neuromancer.visuals import (
@@ -25,38 +26,41 @@ import common.system_id as sys_id
 import common.control as ctl
 
 if __name__ == "__main__":
-    args_id = sys_id.get_parser().parse_args()
-    print({k: str(getattr(args_id, k)) for k in vars(args_id) if getattr(args_id, k)})
-    device = f"cuda:{args_id.gpu}" if args_id.gpu is not None else "cpu"
+    parser = sys_id.get_parser()
+    parser = ctl.get_parser(parser=parser, add_prefix=True)
+    args = parser.parse_args()
+
+    print({k: str(getattr(args, k)) for k in vars(args) if getattr(args, k)})
+    device = f"cuda:{args.gpu}" if args.gpu is not None else "cpu"
 
     # Load Dataset System ID
-    logger_id = get_logger(args_id)
-    dataset_id = load_dataset(args_id, device)
-    print(dataset_id.dims)
+    logger = get_logger(args)
+    dataset = load_dataset(args, device)
+    print(dataset.dims)
 
     # System ID Model
-    estimator, dynamics_model = sys_id.get_model_components(args_id, dataset_id)
-    objectives_id, constraints_id = sys_id.get_objective_terms(
-        args_id, dataset_id, estimator, dynamics_model
+    estimator, dynamics_model = sys_id.get_model_components(args, dataset)
+    objectives, constraints = sys_id.get_objective_terms(
+        args, dataset, estimator, dynamics_model
     )
 
-    model_id = Problem(objectives_id, constraints_id, [estimator, dynamics_model])
-    model_id = model_id.to(device)
+    model = Problem(objectives, constraints, [estimator, dynamics_model])
+    model = model.to(device)
 
-    simulator_ol = OpenLoopSimulator(
-        model=model_id, dataset=dataset_id, eval_sim=not args_id.skip_eval_sim
+    simulator = OpenLoopSimulator(
+        model=model, dataset=dataset, eval_sim=not args.skip_eval_sim
     )
-    optimizer = torch.optim.AdamW(model_id.parameters(), lr=args_id.lr)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
     trainer = Trainer(
-        model_id,
-        dataset_id,
+        model,
+        dataset,
         optimizer,
-        logger=logger_id,
-        simulator=simulator_ol,
-        epochs=args_id.epochs,
-        eval_metric=args_id.eval_metric,
-        patience=args_id.patience,
-        warmup=args_id.warmup,
+        logger=logger,
+        simulator=simulator,
+        epochs=args.epochs,
+        eval_metric=args.eval_metric,
+        patience=args.patience,
+        warmup=args.warmup,
     )
 
     # System ID train
@@ -64,36 +68,45 @@ if __name__ == "__main__":
     best_outputs = trainer.evaluate(best_model)
 
     visualizer_system_ID = VisualizerOpen(
-        dataset_id,
+        dataset,
         dynamics_model,
-        args_id.verbosity,
-        args_id.savedir,
-        training_visuals=args_id.train_visuals,
-        trace_movie=args_id.trace_movie,
+        args.verbosity,
+        args.savedir,
+        training_visuals=args.train_visuals,
+        trace_movie=args.trace_movie,
     )
-    plots_id = visualizer_system_ID.eval(best_outputs)
+    plots = visualizer_system_ID.eval(best_outputs)
 
-    # Logger
-    logger_id.log_artifacts(plots_id)
-    logger_id.clean_up()
+    logger.log_artifacts(plots)
+    logger.clean_up()
+
+    ## Control Policy Learning:
+
+    ctrl_args = argparse.Namespace(
+        **{
+            **{k: v for k, v in vars(args).items() if not k.startswith("ctrl_")},
+            **{
+                k.replace("ctrl_", ""): v
+                for k, v in vars(args).items()
+                if k.startswith("ctrl_")
+            },
+        }
+    )
 
     # Control Dataset
-    args_ctrl = ctl.get_parser().parse_args()
-    print({k: str(getattr(args_id, k)) for k in vars(args_id) if getattr(args_id, k)})
-    device = f"cuda:{args_ctrl.gpu}" if args_ctrl.gpu is not None else "cpu"
-    logger_ctrl = get_logger(args_ctrl)
-    dataset_ctrl = load_dataset(args_ctrl, device)
-    dataset_ctrl = ctl.add_reference_features(dataset_ctrl, dynamics_model)
+    logger = get_logger(ctrl_args)
+    dataset = load_dataset(ctrl_args, device)
+    dataset = ctl.add_reference_features(dataset, dynamics_model)
 
     # Control Problem Definition
     estimator, dynamics_model = ctl.update_system_id_inputs(
-        args_ctrl, dataset_ctrl, estimator, dynamics_model
+        ctrl_args, dataset, estimator, dynamics_model
     )
     policy = ctl.get_policy_components(
-        args_ctrl, dataset_ctrl, dynamics_model, policy_name="policy"
+        ctrl_args, dataset, dynamics_model, policy_name="policy"
     )
     signal_generator = WhiteNoisePeriodicGenerator(
-        args_ctrl.nsteps,
+        ctrl_args.nsteps,
         dynamics_model.fy.out_features,
         xmax=(0.8, 0.7),
         xmin=0.2,
@@ -105,46 +118,44 @@ if __name__ == "__main__":
         ratio=0.05, keys=["Y_pred_dynamics"], name="_noise"
     )
 
-    objectives, constraints = ctl.get_objective_terms(args_ctrl, policy)
-    model_ctrl = Problem(
+    objectives, constraints = ctl.get_objective_terms(ctrl_args, policy)
+    model = Problem(
         objectives,
         constraints,
         [signal_generator, estimator, policy, dynamics_model, noise_generator],
     )
-    model_ctrl = model_ctrl.to(device)
+    model = model.to(device)
 
     # train only policy component
-    freeze_weight(model_ctrl, module_names=args_ctrl.freeze)
-    unfreeze_weight(model_ctrl, module_names=args_ctrl.unfreeze)
-    optimizer = torch.optim.AdamW(model_ctrl.parameters(), lr=args_ctrl.lr)
+    freeze_weight(model, module_names=ctrl_args.freeze)
+    unfreeze_weight(model, module_names=ctrl_args.unfreeze)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=ctrl_args.lr)
 
     plot_keys = ["Y_pred", "U_pred"]  # variables to be plotted
     visualizer = VisualizerClosedLoop(
-        dataset_ctrl, policy, plot_keys, args_ctrl.verbosity, savedir=args_ctrl.savedir
+        dataset, policy, plot_keys, args.verbosity, savedir=args.savedir
     )
 
     policy.input_keys[0] = "Yp"  # hack for policy input key compatibility w/ simulator
     simulator = ClosedLoopSimulator(
-        model=model_ctrl, dataset=dataset_ctrl, emulator=dynamics_model, policy=policy
+        model=model, dataset=dataset, emulator=dynamics_model, policy=policy
     )
     trainer = Trainer(
-        model_ctrl,
-        dataset_ctrl,
+        model,
+        dataset,
         optimizer,
-        logger=logger_ctrl,
+        logger=logger,
         visualizer=visualizer,
         simulator=simulator,
-        epochs=args_ctrl.epochs,
-        patience=args_ctrl.patience,
-        warmup=args_ctrl.warmup,
+        epochs=ctrl_args.epochs,
+        patience=ctrl_args.patience,
+        warmup=ctrl_args.warmup,
     )
 
     # Train control policy
     best_model = trainer.train()
     best_outputs = trainer.evaluate(best_model)
-    plots_ctrl = visualizer.eval(best_outputs)
+    plots = visualizer.eval(best_outputs)
 
-    # Logger
-    logger_ctrl.log_artifacts(plots_ctrl)
-    logger_ctrl.log_metrics({"alive": 0.0})
-    logger_ctrl.clean_up()
+    logger.log_artifacts(plots)
+    logger.clean_up()

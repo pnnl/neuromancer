d229e74d7a9486061ffe66e707154ac776af5ccd
Jan Drgona <jan.drgona@pnnl.gov>
2020-12-30 11:07:10 -0800

    update activation plots

--- neuromancer/train_scripts/papers/stability_l4dc/mk_activation_plots.py
+++ neuromancer/train_scripts/papers/stability_l4dc/mk_activation_plots.py
@@ -27,15 +27,36 @@ def get_colors(k):
         .5 * (1. + np.cos(phi - 2 * np.pi / 3)))).T  # Shape = (60,3)
     return rgb_cycle
 
+def plot_act(x_vec, g, color=[1., 0.25, 0.25], fname=None):
+    plt.style.use('classic')
+    fig, (axe) = plt.subplots(1, 1)
+    axe.set_ylim(-6, 6)
+    axe.set_xlim(-6, 6)
+    axe.set_aspect(1)
+    axe.fill_between(x_vec, x_vec, -x_vec, alpha=0.1)
+    for k in range(len(X_stable)):
+        axe.plot(x_vec, g, c=color, linewidth=2)
+    axe.tick_params(axis='x', labelsize=18)
+    axe.tick_params(axis='y', labelsize=18)
+    axe.grid(True)
+    plt.xlabel('$z$', fontsize=22)
+    plt.ylabel('$g(z)$', fontsize=22)
+    plt.tight_layout()
+    if fname is not None:
+        plt.savefig(fname)
+        plt.close()
 
 
 if __name__ == '__main__':
+
+    outdir = "plots_20201230_activations"
+    os.makedirs(outdir, exist_ok=True)
 
     x_line = torch.range(-6, 6, step=0.01)
     x_line = x_line[x_line != 0.0]  # drop 0 due to division by 0
     x_vec = x_line.detach().numpy()
 
-    activations = []
+    activations = [torch.nn.Identity()]
     for name, act in nact.activations.items():
         # print(name)
         f = act()
@@ -47,6 +68,7 @@ if __name__ == '__main__':
     Act_stable = []
     Act_BIBO_stable = []
     Act_unstable = []
+
     for act in activations:
         x = act(x_line).detach().numpy()
         if any(abs(x/x_vec) > 1):
@@ -66,6 +88,7 @@ if __name__ == '__main__':
         else:
             X_stable.append(x)
             Act_stable.append(act)
+        plot_act(x_vec, x, fname=os.path.join(outdir, f"act_{str(act)}.png"))
 
     print(f'unstable activations: {Act_unstable}')
     print(f'stable activations: {Act_stable}')
@@ -83,7 +106,11 @@ if __name__ == '__main__':
     axe.tick_params(axis='x', labelsize=18)
     axe.tick_params(axis='y', labelsize=18)
     axe.grid(True)
+    plt.xlabel('$z$', fontsize=22)
+    plt.ylabel('$g(z)$', fontsize=22)
     plt.tight_layout()
+    plt.savefig(os.path.join(outdir, f"act_stable.png"))
+
 
     # Unstable activations
     rgb_colors = get_colors(len(X_unstable)+1)
@@ -98,7 +125,10 @@ if __name__ == '__main__':
     axe.tick_params(axis='x', labelsize=18)
     axe.tick_params(axis='y', labelsize=18)
     axe.grid(True)
+    plt.xlabel('$z$', fontsize=22)
+    plt.ylabel('$g(z)$', fontsize=22)
     plt.tight_layout()
+    plt.savefig(os.path.join(outdir, f"act_unstable.png"))
 
     # BIBO stable activations
     rgb_colors = get_colors(len(X_BIBO_stable)+1)
@@ -113,7 +143,10 @@ if __name__ == '__main__':
     axe.tick_params(axis='x', labelsize=18)
     axe.tick_params(axis='y', labelsize=18)
     axe.grid(True)
+    plt.xlabel('$z$', fontsize=22)
+    plt.ylabel('$g(z)$', fontsize=22)
     plt.tight_layout()
+    plt.savefig(os.path.join(outdir, f"act_bibo_stable.png"))
 
 
 

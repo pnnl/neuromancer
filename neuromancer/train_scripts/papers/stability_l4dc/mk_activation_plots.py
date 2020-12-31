8583c3851a649b4d64f22c2cbcc88b5957e88a90
Jan Drgona <jan.drgona@pnnl.gov>
2020-12-31 09:46:23 -0800

    update

--- neuromancer/train_scripts/papers/stability_l4dc/mk_activation_plots.py
+++ neuromancer/train_scripts/papers/stability_l4dc/mk_activation_plots.py
@@ -72,23 +72,20 @@ if __name__ == '__main__':
     for act in activations:
         x = act(x_line).detach().numpy()
         if any(abs(x/x_vec) > 1):
-            # x_1 = act(torch.tensor([5.0])).detach().numpy()
+            X_unstable.append(x)
+            Act_unstable.append(act)
+            # x_1 = act(torch.tensor([-6.0])).detach().numpy()
             # x_2 = act(torch.tensor([6.0])).detach().numpy()
-            # x_3 = act(torch.tensor([-5.0])).detach().numpy()
-            # x_4 = act(torch.tensor([-6.0])).detach().numpy()
-            # if abs(x_1 - x_2).squeeze() <= 0.01 and abs(x_3 - x_4).squeeze() <= 0.01:
-            x_1 = act(torch.tensor([-6.0])).detach().numpy()
-            x_2 = act(torch.tensor([6.0])).detach().numpy()
-            if abs(x_1).squeeze() < 6.0 and abs(x_2).squeeze() < 6.0:
-                X_BIBO_stable.append(x)
-                Act_BIBO_stable.append(act)
-            else:
-                X_unstable.append(x)
-                Act_unstable.append(act)
+            # if abs(x_1).squeeze() < 6.0 and abs(x_2).squeeze() < 6.0:
+            #     X_BIBO_stable.append(x)
+            #     Act_BIBO_stable.append(act)
+            # else:
+            #     X_unstable.append(x)
+            #     Act_unstable.append(act)
         else:
             X_stable.append(x)
             Act_stable.append(act)
-        plot_act(x_vec, x, fname=os.path.join(outdir, f"act_{str(act)}.png"))
+        # plot_act(x_vec, x, fname=os.path.join(outdir, f"act_{str(act)}.png"))
 
     print(f'unstable activations: {Act_unstable}')
     print(f'stable activations: {Act_stable}')
@@ -130,23 +127,23 @@ if __name__ == '__main__':
     plt.tight_layout()
     plt.savefig(os.path.join(outdir, f"act_unstable.png"))
 
-    # BIBO stable activations
-    rgb_colors = get_colors(len(X_BIBO_stable)+1)
-    plt.style.use('classic')
-    fig, (axe) = plt.subplots(1, 1)
-    axe.set_ylim(-6, 6)
-    axe.set_xlim(-6, 6)
-    axe.set_aspect(1)
-    axe.fill_between(x_vec, x_vec, -x_vec, alpha=0.1)
-    for k in range(len(X_BIBO_stable)):
-        axe.plot(x_vec, X_BIBO_stable[k], c=rgb_colors[k], linewidth=2)
-    axe.tick_params(axis='x', labelsize=18)
-    axe.tick_params(axis='y', labelsize=18)
-    axe.grid(True)
-    plt.xlabel('$z$', fontsize=22)
-    plt.ylabel('$g(z)$', fontsize=22)
-    plt.tight_layout()
-    plt.savefig(os.path.join(outdir, f"act_bibo_stable.png"))
+    # # BIBO stable activations
+    # rgb_colors = get_colors(len(X_BIBO_stable)+1)
+    # plt.style.use('classic')
+    # fig, (axe) = plt.subplots(1, 1)
+    # axe.set_ylim(-6, 6)
+    # axe.set_xlim(-6, 6)
+    # axe.set_aspect(1)
+    # axe.fill_between(x_vec, x_vec, -x_vec, alpha=0.1)
+    # for k in range(len(X_BIBO_stable)):
+    #     axe.plot(x_vec, X_BIBO_stable[k], c=rgb_colors[k], linewidth=2)
+    # axe.tick_params(axis='x', labelsize=18)
+    # axe.tick_params(axis='y', labelsize=18)
+    # axe.grid(True)
+    # plt.xlabel('$z$', fontsize=22)
+    # plt.ylabel('$g(z)$', fontsize=22)
+    # plt.tight_layout()
+    # plt.savefig(os.path.join(outdir, f"act_bibo_stable.png"))
 
 
 

c415d24df3121aa1f6408c4e9842ffa01e9c4bfe
Jan Drgona <jan.drgona@pnnl.gov>
2021-08-12 08:37:54 -0700

    update args in system id tutorials
    Former-commit-id: 159c43de69272ee4be88aad8d69bc7fa87fa7402

--- examples/system identification/system_id_basic.py
+++ examples/system identification/system_id_basic.py
@@ -41,6 +41,99 @@ from neuromancer.dataset import read_file, normalize_data, split_sequence_data, 
 from torch.utils.data import DataLoader
 
 
+
+def arg_sys_id_problem(prefix='', system='CSTR'):
+    """
+    Command line parser for system identification problem arguments
+
+    :param prefix: (str) Optional prefix for command line arguments to resolve naming conflicts when multiple parsers
+                         are bundled as parents.
+    :return: (arg.ArgParse) A command line parser
+    """
+    parser = arg.ArgParser(prefix=prefix, add_help=False)
+    gp = parser.group("system_id")
+    #  DATA
+    gp.add("-dataset", type=str, default=system,
+           help="select particular dataset with keyword")
+    gp.add("-nsteps", type=int, default=32,
+           help="prediction horizon.")
+    gp.add("-nsim", type=int, default=10000,
+           help="Number of time steps for full dataset. (ntrain + ndev + ntest)"
+                "train, dev, and test will be split evenly from contiguous, sequential, "
+                "non-overlapping chunks of nsim datapoints, e.g. first nsim/3 art train,"
+                "next nsim/3 are dev and next nsim/3 simulation steps are test points."
+                "None will use a default nsim from the selected dataset or emulator")
+    gp.add("-norm", nargs="+", default=["U", "D", "Y"], choices=["U", "D", "Y", "X"],
+           help="List of sequences to max-min normalize")
+    gp.add("-data_seed", type=int, default=408,
+           help="Random seed used for simulated data")
+    #  COMPONENTS
+    gp.add("-ssm_type", type=str, choices=["blackbox", "hw", "hammerstein",
+                                "blocknlin", "linear"], default="hammerstein",
+           help='Choice of block structure for system identification model')
+    gp.add("-state_estimator", type=str, choices=["rnn", "mlp", "linear",
+                            "residual_mlp", "fully_observable"], default="mlp",
+           help='Choice of model architecture for state estimator.')
+    gp.add("-estimator_input_window", type=int, default=8,
+           help="Number of previous time steps measurements to include in state estimator input")
+    gp.add("-nx_hidden", type=int, default=32,
+           help="Number of hidden states per output")
+    gp.add("-n_layers", type=int, default=2,
+           help="Number of hidden layers of single time-step state transition")
+    gp.add("-nonlinear_map", type=str, default="mlp", choices=["mlp", "rnn", "pytorch_rnn",
+                                                               "linear", "residual_mlp"],
+           help='Choice of architecture for component blocks in state space model.')
+    gp.add("-bias", action="store_true",
+           help="Whether to use bias in the neural network block component models.")
+    gp.add("-activation", choices=activations.keys(), default="gelu",
+           help="Activation function for component block neural networks")
+    gp.add("-linear_map", type=str, choices=["linear", "softSVD", "pf"], default="linear",
+           help='Choice of map from SLiM package')
+    gp.add("-sigma_min", type=float, default=0.1,
+           help='Minimum singular value (for maps with singular value constraints)')
+    gp.add("-sigma_max", type=float, default=1.0,
+           help='Maximum singular value (for maps with singular value constraints)')
+    #  LOSS
+    gp.add("-Q_con_x", type=float, default=1.0,
+           help="Hidden state constraints penalty weight.")
+    gp.add("-Q_dx", type=float, default=0.1,
+           help="Penalty weight on hidden state difference in one time step.")
+    gp.add("-Q_sub", type=float, default=0.1,
+           help="Linear maps regularization weight.")
+    gp.add("-Q_y", type=float, default=1.0,
+           help="Output tracking penalty weight")
+    gp.add("-Q_e", type=float, default=1.0,
+           help="State estimator hidden prediction penalty weight")
+    gp.add("-Q_con_fdu", type=float, default=0.0,
+           help="Penalty weight on control actions and disturbances.")
+    #  LOG
+    gp.add("-logger", type=str, choices=["mlflow", "stdout"], default="stdout",
+           help="Logging setup to use")
+    gp.add("-savedir", type=str, default="test",
+           help="Where should your trained model and plots be saved (temp)")
+    gp.add("-verbosity", type=int, default=1,
+           help="How many epochs in between status updates")
+    gp.add("-metrics", nargs="+", default=["nstep_dev_loss", "loop_dev_loss", "best_loop_dev_loss",
+               "nstep_dev_ref_loss", "loop_dev_ref_loss"],
+           help="Metrics to be logged")
+    #  OPTIMIZATION
+    gp.add("-eval_metric", type=str, default="loop_dev_ref_loss",
+            help="Metric for model selection and early stopping.")
+    gp.add("-epochs", type=int, default=300,
+           help='Number of training epochs')
+    gp.add("-lr", type=float, default=0.001,
+           help="Step size for gradient descent.")
+    gp.add("-patience", type=int, default=100,
+           help="How many epochs to allow for no improvement in eval metric before early stopping.")
+    gp.add("-warmup", type=int, default=10,
+           help="Number of epochs to wait before enacting early stopping policy.")
+    gp.add("-skip_eval_sim", action="store_true",
+           help="Whether to run simulator during evaluation phase of training.")
+    gp.add("-seed", type=int, default=408, help="Random seed used for weight initialization.")
+    parser.add("-gpu", type=int, help="GPU to use")
+    return parser
+
+
 def get_sequence_dataloaders(
     data, nsteps, moving_horizon=False, norm_type="zero-one", split_ratio=None, num_workers=0,
 ):
@@ -263,23 +356,14 @@ if __name__ == "__main__":
     # for available systems in PSL library check: psl.systems.keys()
     # for available datasets in PSL library check: psl.datasets.keys()
     system = 'aero'         # keyword of selected system
-    parser = arg.ArgParser(parents=[arg.log(), arg.opt(), arg.data(system=system),
-                                    arg.loss(), arg.lin(), arg.ssm()])
-
-    grp = parser.group('OPTIMIZATION')
-    grp.add("-eval_metric", type=str, default="loop_dev_ref_loss",
-            help="Metric for model selection and early stopping.")
+    parser = arg.ArgParser(parents=[arg_sys_id_problem(system=system)])
     args, grps = parser.parse_arg_groups()
     print({k: str(getattr(args, k)) for k in vars(args) if getattr(args, k)})
-
     device = f"cuda:{args.gpu}" if args.gpu is not None else "cpu"
-
     log_constructor = MLFlowLogger if args.logger == 'mlflow' else BasicLogger
-    metrics = ["nstep_dev_loss", "loop_dev_loss", "best_loop_dev_loss",
-               "nstep_dev_ref_loss", "loop_dev_ref_loss"]
-    logger = log_constructor(args=args, savedir=args.savedir, verbosity=args.verbosity, stdout=metrics)
+    logger = log_constructor(args=args, savedir=args.savedir, verbosity=args.verbosity, stdout=args.metrics)
 
-    # TODO: this can probably be moved to dataset.py
+    # load raw data
     if args.dataset in psl.emulators:
         data = psl.emulators[args.dataset](nsim=args.nsim, ninit=0, seed=args.data_seed).simulate()
     elif args.dataset in psl.datasets:
@@ -287,18 +371,21 @@ if __name__ == "__main__":
     else:
         data = read_file(args.dataset)
 
+    # create dataloaders from raw data
     nstep_data, loop_data, dims = get_sequence_dataloaders(data, args.nsteps)
     train_data, dev_data, test_data = nstep_data
     train_loop, dev_loop, test_loop = loop_data
 
+    # create component models, objectives, and constraints
     estimator, dynamics_model = get_model_components(args, dims)
     objectives, constraints = get_objective_terms(args, dims, estimator, dynamics_model)
 
+    # create problem
     model = Problem(objectives, constraints, [estimator, dynamics_model])
     model = model.to(device)
-
     print(model)
 
+    # create callback
     simulator = OpenLoopSimulator(
         model, train_loop, dev_loop, test_loop, eval_sim=not args.skip_eval_sim, device=device,
     ) if isinstance(train_loop, dict) else MultiSequenceOpenLoopSimulator(
@@ -311,10 +398,10 @@ if __name__ == "__main__":
         training_visuals=False,
         trace_movie=False,
     )
-
     callback = SysIDCallback(simulator, visualizer)
+    # select optimizer
     optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
-
+    # create trainer
     trainer = Trainer(
         model,
         train_data,
@@ -329,7 +416,7 @@ if __name__ == "__main__":
         warmup=args.warmup,
         device=device,
     )
-
+    # train
     best_model = trainer.train()
     best_outputs = trainer.test(best_model)
     logger.clean_up()
--- examples/tutorials/system_id_tutorial.py
+++ examples/tutorials/system_id_tutorial.py
@@ -1,6 +1,7 @@
 
 import torch
 import torch.nn.functional as F
+from torch.utils.data import DataLoader
 import slim
 import psl
 
@@ -14,8 +15,71 @@ from neuromancer.simulators import OpenLoopSimulator, MultiSequenceOpenLoopSimul
 from neuromancer.callbacks import SysIDCallback
 from neuromancer.loggers import BasicLogger, MLFlowLogger
 from neuromancer.dataset import read_file, normalize_data, split_sequence_data, SequenceDataset
-from torch.utils.data import DataLoader
 from neuromancer.constraint import Variable
+
+
+
+def arg_sys_id_problem(prefix='', system='CSTR'):
+    """
+    Command line parser for system identification problem arguments
+
+    :param prefix: (str) Optional prefix for command line arguments to resolve naming conflicts when multiple parsers
+                         are bundled as parents.
+    :return: (arg.ArgParse) A command line parser
+    """
+    parser = arg.ArgParser(prefix=prefix, add_help=False)
+    gp = parser.group("system_id")
+    #  DATA
+    gp.add("-dataset", type=str, default=system,
+           help="select particular dataset with keyword")
+    gp.add("-nsteps", type=int, default=32,
+           help="prediction horizon.")
+    gp.add("-nsim", type=int, default=10000,
+           help="Number of time steps for full dataset. (ntrain + ndev + ntest)"
+                "train, dev, and test will be split evenly from contiguous, sequential, "
+                "non-overlapping chunks of nsim datapoints, e.g. first nsim/3 art train,"
+                "next nsim/3 are dev and next nsim/3 simulation steps are test points."
+                "None will use a default nsim from the selected dataset or emulator")
+    gp.add("-norm", nargs="+", default=["U", "D", "Y"], choices=["U", "D", "Y", "X"],
+           help="List of sequences to max-min normalize")
+    gp.add("-data_seed", type=int, default=408,
+           help="Random seed used for simulated data")
+    #  LOSS
+    gp.add("-Q_con_x", type=float, default=1.0,
+           help="Hidden state constraints penalty weight.")
+    gp.add("-Q_dx", type=float, default=0.1,
+           help="Penalty weight on hidden state difference in one time step.")
+    gp.add("-Q_sub", type=float, default=0.1,
+           help="Linear maps regularization weight.")
+    gp.add("-Q_y", type=float, default=1.0,
+           help="Output tracking penalty weight")
+    gp.add("-Q_e", type=float, default=1.0,
+           help="State estimator hidden prediction penalty weight")
+    #  LOG
+    gp.add("-savedir", type=str, default="test",
+           help="Where should your trained model and plots be saved (temp)")
+    gp.add("-verbosity", type=int, default=1,
+           help="How many epochs in between status updates")
+    gp.add("-metrics", nargs="+", default=["nstep_dev_loss", "loop_dev_loss", "best_loop_dev_loss",
+               "nstep_dev_ref_loss", "loop_dev_ref_loss"],
+           help="Metrics to be logged")
+    #  OPTIMIZATION
+    gp.add("-eval_metric", type=str, default="loop_dev_ref_loss",
+            help="Metric for model selection and early stopping.")
+    gp.add("-epochs", type=int, default=300,
+           help='Number of training epochs')
+    gp.add("-lr", type=float, default=0.001,
+           help="Step size for gradient descent.")
+    gp.add("-patience", type=int, default=100,
+           help="How many epochs to allow for no improvement in eval metric before early stopping.")
+    gp.add("-warmup", type=int, default=10,
+           help="Number of epochs to wait before enacting early stopping policy.")
+    gp.add("-skip_eval_sim", action="store_true",
+           help="Whether to run simulator during evaluation phase of training.")
+    gp.add("-seed", type=int, default=408, help="Random seed used for weight initialization.")
+    gp.add("-device", type=str, default="cpu", choices=["cpu", "gpu"],
+           help="select device")
+    return parser
 
 
 def get_sequence_dataloaders(
@@ -97,21 +161,10 @@ if __name__ == "__main__":
     # for available systems in PSL library check: psl.systems.keys()
     # for available datasets in PSL library check: psl.datasets.keys()
     system = 'aero'         # keyword of selected system
-
     # load argument parser
-    parser = arg.ArgParser(parents=[arg.log(), arg.opt(), arg.data(system=system),
-                                    arg.loss(), arg.lin(), arg.ssm()])
-    grp = parser.group('OPTIMIZATION')
-    grp.add("-eval_metric", type=str, default="loop_dev_ref_loss",
-            help="Metric for model selection and early stopping.")
+    parser = arg.ArgParser(parents=[arg_sys_id_problem(system=system)])
     args, grps = parser.parse_arg_groups()
-    args.nsteps = 32  # define prediction horizon length
-
-    device = "cpu"
-    # metrics to be logged
-    metrics = ["nstep_dev_loss", "loop_dev_loss", "best_loop_dev_loss",
-               "nstep_dev_ref_loss", "loop_dev_ref_loss"]
-    logger = BasicLogger(args=args, savedir=args.savedir, verbosity=args.verbosity, stdout=metrics)
+    logger = BasicLogger(args=args, savedir=args.savedir, verbosity=args.verbosity, stdout=args.metrics)
 
     """
     # # # # # # # # # # # # # # # # # # #
@@ -183,11 +236,11 @@ if __name__ == "__main__":
     # neural network blocks
     fx = blocks.RNN(nx, nx, linear_map=linmap,
                     nonlin=activations['softexp'], hsizes=[60, 60])
-    linargs = {"sigma_min": 0.95, "sigma_max": 1.0}
+    linargs = {"sigma_min": 0.5, "sigma_max": 1.0}
     fy = slim.maps['softSVD'](nx, ny, linargs=linargs)
     fu = blocks.MLP(nu, nx, hsizes=[60, 60]) if nu != 0 else None
 
-    dynamics_model = dynamics.BlockSSM(fx, fy, fu=fu, name='dynamics',
+    dynamics_model = dynamics.BlockSSM(fx, fy, fu=fu, name='dynamics', xou=torch.add,
                                        input_keys={f"x0_{estimator.name}": "x0"})
     # Yf = dynamics_model(x0, Uf)
     # Uf: future control actions
@@ -248,7 +301,7 @@ if __name__ == "__main__":
     # estimator -> dynamics_model
     components = [estimator, dynamics_model]
     model = Problem(objectives, constraints, components)
-    model = model.to(device)
+    model = model.to(args.device)
 
 
     """    
@@ -258,12 +311,11 @@ if __name__ == "__main__":
 
     #  trainer = problem + datasets + optimizer + callback
     """
-    args.epochs = 300
-
+    # define callback
     simulator = OpenLoopSimulator(
-        model, train_loop, dev_loop, test_loop, eval_sim=not args.skip_eval_sim, device=device,
+        model, train_loop, dev_loop, test_loop, eval_sim=not args.skip_eval_sim, device=args.device,
     ) if isinstance(train_loop, dict) else MultiSequenceOpenLoopSimulator(
-        model, train_loop, dev_loop, test_loop, eval_sim=not args.skip_eval_sim, device=device,
+        model, train_loop, dev_loop, test_loop, eval_sim=not args.skip_eval_sim, device=args.device,
     )
     visualizer = VisualizerOpen(
         dynamics_model,
@@ -273,8 +325,9 @@ if __name__ == "__main__":
         trace_movie=False,
     )
     callback = SysIDCallback(simulator, visualizer)
+    # select optimizer
     optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
-
+    # define trainer
     trainer = Trainer(
         model,
         train_data,
@@ -287,9 +340,9 @@ if __name__ == "__main__":
         eval_metric=args.eval_metric,
         patience=args.patience,
         warmup=args.warmup,
-        device=device,
+        device=args.device,
     )
-
+    # train the model
     best_model = trainer.train()
 
     """    
@@ -305,42 +358,3 @@ if __name__ == "__main__":
     # visualizer.eval(best_outputs)
     logger.clean_up()
 
-
-    # # # Alternative Loss terms and constraitns definition via Objective class:
-    # # # this approach can be used for more user flexibility as it supports any callable
-
-    # reference_loss = Objective(
-    #     [f"Y_pred_{dynamics_model.name}", "Yf"], F.mse_loss, weight=args.Q_y, name="ref_loss"
-    # )
-    #
-    # estimator_loss = Objective(
-    #     [f"X_pred_{dynamics_model.name}", f"x0_{estimator.name}"],
-    #     lambda X_pred, x0: F.mse_loss(X_pred[-1, :-1, :], x0[1:]),
-    #     weight=args.Q_e,
-    #     name="arrival_cost",
-    # )
-    # regularization = Objective(
-    #     [f"reg_error_{estimator.name}", f"reg_error_{dynamics_model.name}"],
-    #     lambda reg1, reg2: reg1 + reg2,
-    #     weight=args.Q_sub,
-    #     name="reg_error",
-    # )
-    # state_smoothing = Objective(
-    #     [f"X_pred_{dynamics_model.name}"],
-    #     lambda x: F.mse_loss(x[1:], x[:-1]),
-    #     weight=args.Q_dx,
-    #     name="state_smoothing",
-    # )
-    # # inequality constraints
-    # observation_lower_bound_penalty = Objective(
-    #     [f"Y_pred_{dynamics_model.name}"],
-    #     lambda x: torch.mean(F.relu(-x + xmin)),
-    #     weight=args.Q_con_x,
-    #     name="y_low_bound_error",
-    # )
-    # observation_upper_bound_penalty = Objective(
-    #     [f"Y_pred_{dynamics_model.name}"],
-    #     lambda x: torch.mean(F.relu(x - xmax)),
-    #     weight=args.Q_con_x,
-    #     name="y_up_bound_error",
-    # )

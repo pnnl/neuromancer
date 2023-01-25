23e2b78b1500257709d3c0a7b5aa0bfda2250246
Ted <oancea.tudor@icloud.com>
2023-01-25 14:37:07 +0100

    added requirements file and github action workflow

--- setup.py
+++ setup.py
@@ -1,16 +1,36 @@
+try:
+    import torch
+except ImportError:
+    raise ImportError("Please install torch before installing neuromancer")
+
 from setuptools import setup, find_packages
+
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read().splitlines()
+    # remove lines that start with #
+    requirements = [
+        r
+        for r in requirements
+        if not (r.startswith("#") or r.startswith("-e git+") or r.startswith("git+"))
+    ]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 
 setup(
     name="neuromancer",
     version="1.3",
     description="Neural Modules with Adaptive Nonlinear Constraints and Efficient Regularization",
+    long_description=long_description,
     url="https://pnnl.github.io/neuromancer/",
     author="Aaron Tuor, Jan Drgona, Mia Skomski, Stefan Dernbach, James Koch, Zhao Chen, Christian Møldrup Legaard, Draguna Vrabie",
     author_email="aaron.tuor@pnnl.gov",
     license="BSD2",
     packages=find_packages(),  # or list of package paths from this directory
     zip_safe=False,
-    classifiers=['Programming Language :: Python :: 3.10'],
+    classifiers=["Programming Language :: Python :: 3.10"],
+    install_requires=requirements,
     keywords=[
         "Deep Learning",
         "Pytorch",

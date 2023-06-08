53c0d564202b40a0ab084a0424a7113069d8db64
Aaron Tuor <aaron.tuor@pnnl.gov>
2023-06-08 11:48:43 -0700

    tutorials

--- examples/system_identification/brusselator_parameter.py
+++ examples/system_identification/brusselator_parameter.py
@@ -2,131 +2,152 @@
 Parameter estimation for a 1D Brusselator system.
 """
 import torch
-import neuromancer.slim as slim
-import neuromancer.psl as psl
-import numpy as np
+from neuromancer.psl import plot
+from neuromancer import psl
 import matplotlib.pyplot as plt
 import argparse
+from torch.utils.data import DataLoader
+import os
 
-from neuromancer import blocks, estimators, dynamics, arg, integrators, ode
+from neuromancer.system import Node, System
+from neuromancer.dynamics import integrators, ode
+from neuromancer.dynamics.ode import ode_param_systems_auto as systems
 from neuromancer.trainer import Trainer
 from neuromancer.problem import Problem
 from neuromancer.loggers import BasicLogger
-from neuromancer.dataset import get_sequence_dataloaders
+from neuromancer.dataset import DictDataset
 from neuromancer.constraint import variable
 from neuromancer.loss import PenaltyLoss
-import neuromancer.simulator as sim
+
+
+def get_data(sys, nsim, nsteps, bs):
+    """
+    :param nsteps: (int) Number of timesteps for each batch of training data
+    :param sys: (psl.ODE_NonAutonomous)
+    :param normalize: (bool) Whether to normalize the data
+
+    """
+    train_sim, dev_sim, test_sim = [sys.simulate(nsim=nsim, ts=0.05) for i in range(3)]
+    nx = sys.nx
+    nbatch = nsim//nsteps
+    length = (nsim//nsteps) * nsteps
+
+    trainX = train_sim['X'][:length].reshape(nbatch, nsteps, nx)
+    trainX = torch.tensor(trainX, dtype=torch.float32)
+    train_data = DictDataset({'X': trainX, 'xn': trainX[:, 0:1, :]}, name='train')
+    train_loader = DataLoader(train_data, batch_size=bs,
+                              collate_fn=train_data.collate_fn, shuffle=True)
+
+    devX = dev_sim['X'][:length].reshape(nbatch, nsteps, nx)
+    devX = torch.tensor(devX, dtype=torch.float32)
+    dev_data = DictDataset({'X': devX, 'xn': devX[:, 0:1, :]}, name='dev')
+    dev_loader = DataLoader(dev_data, batch_size=bs,
+                            collate_fn=dev_data.collate_fn, shuffle=True)
+
+    testX = test_sim['X'][:length].reshape(1, nsim, nx)
+    testX = torch.tensor(testX, dtype=torch.float32)
+    test_data = {'X': testX, 'xn': testX[:, 0:1, :]}
+
+    return train_loader, dev_loader, test_data
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-epochs', type=int, default=100)
+    parser.add_argument('-system', type=str, default='Brusselator')
+
     args = parser.parse_args()
 
     torch.manual_seed(0)
-    device = "cpu"
 
     # %%  ground truth system
     system = psl.systems['Brusselator1D']
     ts = 0.05
     modelSystem = system()
-    raw = modelSystem.simulate(ts=ts)
+    raw = modelSystem.simulate(nsim=1000, ts=ts)
     psl.plot.pltOL(Y=raw['Y'])
     psl.plot.pltPhase(X=raw['Y'])
 
-    # %%  Train, Development, Test sets - nstep and loop format
-    nsteps = 1
-    nstep_data, loop_data, dims = get_sequence_dataloaders(raw, nsteps, moving_horizon=False)
-    train_data, dev_data, test_data = nstep_data
-    train_loop, dev_loop, test_loop = loop_data
+    train_loader, dev_loader, test_data = get_data(modelSystem, 500, 2, 32)
 
-    # %% fully observable estimator
-    nx = 2
-    estim = estimators.FullyObservable(
-        {**train_data.dataset.dims, "x0": (nx,)},
-        linear_map=slim.maps['identity'],
-        input_keys=["Yp"])
-
-    # %% Instantiate the ODE and symbolic dynamics model:
+    nx = modelSystem.nx
     brussels = ode.BrusselatorParam()
     fxRK4 = integrators.RK4(brussels, h=ts)
-    fy = slim.maps['identity'](nx, nx)
-    dynamics_model = dynamics.ODEAuto(fxRK4, fy, name='dynamics',
-                            input_key_map={"x0": estim.output_keys[0]})
+    dynamics_model = System([Node(fxRK4, ['xn'], ['xn'])])
 
     # %% Constraints + losses:
-    yhat = variable(dynamics_model.output_keys[2])
-    y = variable("Yf")
-    x0 = variable(estim.output_keys[0])
-    xhat = variable(dynamics_model.output_keys[1])
+    x = variable("X")
+    xhat = variable('xn')[:, :-1, :]
 
-    yFD = (y[:, 1:, :] - y[:, :-1, :])
-    yhatFD = (yhat[:, 1:, :] - yhat[:, :-1, :])
+    xFD = (x[:, 1:, :] - x[:, :-1, :])
+    xhatFD = (xhat[:, 1:, :] - xhat[:, :-1, :])
 
-
-    fd_loss = 2.0*((yFD == yhatFD)^2)
+    fd_loss = 2.0*((xFD == xhatFD)^2)
     fd_loss.name = 'FD_loss'
 
-    reference_loss = ((yhat == y)^2)
+    reference_loss = ((xhat == x)^2)
     reference_loss.name = "ref_loss"
 
     # %%
     objectives = [reference_loss, fd_loss]
     constraints = []
-    components = [estim, dynamics_model]
     # create constrained optimization loss
     loss = PenaltyLoss(objectives, constraints)
     # construct constrained optimization problem
-    problem = Problem(components, loss)
+    problem = Problem([dynamics_model], loss)
     # plot computational graph
-    problem.plot_graph()
-    problem = problem.to(device)
 
     # %%
     optimizer = torch.optim.Adam(problem.parameters(), lr=0.1)
     logger = BasicLogger(args=None, savedir='test', verbosity=1,
-                         stdout="nstep_dev_"+reference_loss.output_keys[0])
+                         stdout=['dev_loss', 'train_loss'])
 
     trainer = Trainer(
         problem,
-        train_data,
-        dev_data,
+        train_loader,
+        dev_loader,
         test_data,
         optimizer,
         patience=10,
         warmup=10,
         epochs=args.epochs,
-        eval_metric="nstep_dev_"+reference_loss.output_keys[0],
-        train_metric="nstep_train_loss",
-        dev_metric="nstep_dev_loss",
-        test_metric="nstep_test_loss",
+        eval_metric="dev_loss",
+        train_metric="train_loss",
+        dev_metric="dev_loss",
+        test_metric="dev_loss",
         logger=logger,
-        device=device,
     )
     # %%
     best_model = trainer.train()
+    problem.load_state_dict(best_model)
     # %%
-    best_outputs = trainer.test(best_model)
-    # os.system('cp test/open_loop.png figs/brusselator_parameter.png')
     # %%
     print('alpha = '+str(brussels.alpha.item()))
     print('beta = '+str(brussels.beta.item()))
 
-    """
-    Test open loop performance
-    """
-    nm_system = sim.DynamicsNeuromancer(dynamics_model,
-                    name='nm', input_key_map={'x': 'x_nm'})
-    psl_system = sim.DynamicsPSL(modelSystem,
-                    name='psl', input_key_map={'x': 'x_psl'})
-    psl_system.model.ts = ts
-    components = [nm_system, psl_system]
-    system = sim.SystemSimulator(components)
-    x0 = np.ones(nx)
-    data_init = {'x_psl': x0, 'x_nm': x0}
-    trajectories = system.simulate(nsim=400, data_init=data_init)
-    plt.close('all')
-    psl.plot.pltOL(Y=trajectories['y_psl'], Ytrain=trajectories['y_nm'])
-    plt.show(block=True)
-    plt.interactive(False)
+    # Test set results
+    test_outputs = dynamics_model(test_data)
 
+    pred_traj = test_outputs['xn'][:, :-1, :]
+    true_traj = test_data['X']
+    pred_traj = pred_traj.detach().numpy().reshape(-1, nx)
+    true_traj = true_traj.detach().numpy().reshape(-1, nx)
+    pred_traj, true_traj = pred_traj.transpose(1, 0), true_traj.transpose(1, 0)
+
+    figsize = 25
+    plt.xticks(fontsize=figsize)
+    fig, ax = plt.subplots(nx, figsize=(figsize, figsize))
+    labels = [f'$y_{k}$' for k in range(len(true_traj))]
+    for row, (t1, t2, label) in enumerate(zip(true_traj, pred_traj, labels)):
+        if nx > 1:
+            axe = ax[row]
+        else:
+            axe = ax
+        axe.set_ylabel(label, rotation=0, labelpad=20, fontsize=figsize)
+        axe.plot(t1, label='True', c='c')
+        axe.plot(t2, label='Pred', c='m')
+        axe.tick_params(labelbottom=False, labelsize=figsize)
+    axe.tick_params(labelbottom=True, labelsize=figsize)
+    axe.legend(fontsize=figsize)
+    plt.tight_layout()
+    plt.savefig('open_loop.png')

4481f6027cedfe7df72b189feec947144fe1199e
Seth L Briney <SethLBriney@gmail.com>
2023-09-03 13:52:14 -0700

    removed pslgym, corresponding examples and dependencies.

--- tests/psl/test_directory.py
+++
@@ -1,8 +0,0 @@
-import neuromancer as nm
-import os
-
-
-def test_directory():
-    nm_path1 = nm.__path__[0].removesuffix(os.path.join('src', 'neuromancer'))
-    nm_path2 =    os.getcwd().removesuffix(os.path.join('tests', 'psl'))
-    assert os.path.normcase(nm_path1)==os.path.normcase(nm_path2), 'installed neuromancer root directory not the same as test root directory'

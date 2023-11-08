eaa9f1c29a323cec51438ec67e2e99a0dbf01629
Birmiwal, Rahul R <rahul.birmiwal@pnnl.gov>
2023-11-07 16:13:53 -0800

    updaing conf.py system path

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,10 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../neuromancer'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.append(os.path.abspath(
+    os.path.join(__file__, "../../src")
+))
 
 
 # -- Project information -----------------------------------------------------

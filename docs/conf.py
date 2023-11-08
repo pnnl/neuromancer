8054586016257e0abddcb7f865c8bfbb9ef5e594
Birmiwal, Rahul R <rahul.birmiwal@pnnl.gov>
2023-11-08 09:01:24 -0800

    trying to resolve relative import error

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../neuromancer'))
-sys.path.insert(0, os.path.abspath('../src/neuromancer'))
+sys.path.insert(0, os.path.abspath('..'))
+
 
 
 # -- Project information -----------------------------------------------------

5b2421f8edac2e1eac2f3bcfe6d036ad2d2bbfb5
Colby Ham <colby.ham@pnnl.gov>
2024-05-16 16:19:12 -0700

    Add DeepONet class working definitino and usage in notebook

--- src/neuromancer/modules/blocks.py
+++ src/neuromancer/modules/blocks.py
@@ -730,14 +730,21 @@ class DeepONet(Block):
         insize_branch,
         insize_trunk,
         widthsize,
-        interactsize,
+        interactsize, # should this be outsize?
         depth_branch,
         depth_trunk,
         nonlin=nn.ReLU,
         bias=True
     ):
         """
-        TODO param types and descriptions
+        : param insize_branch: (int) dimensionality of branch net input
+        : param insize_trunk: (int) dimensionality of trunk net input
+        : param widthsize: (int) horizontal size for branch and trunk net
+        : param interactsize: (int) dimensionality of branch net and trunk net output
+        : param depth_branch: (int) depth of branch net
+        : param depth_trunk: (int) depth of trunk net
+        : nonlin: (callable) nonlinear activation function for branch net and trunk net
+        : bias: (bool) Whether to use bias
         """
         super().__init__()
         self.branch_net = MLP(
@@ -754,21 +761,26 @@ class DeepONet(Block):
             hsizes=[widthsize] * depth_trunk,
             bias=True
         )
-        # do i need outsize for bias
         self.bias = nn.Parameter(torch.zeros(1,), requires_grad=not bias)
 
     def transpose_branch_inputs(self, branch_inputs):
         transposed_branch_inputs = torch.transpose(branch_inputs, 0, 1)
         return transposed_branch_inputs
 
+
+    def reg_error(self):
+        return self.branch_net.reg_error() + self.trunk_net.reg_error()
+
     def block_eval(self, x):
-        branch_inputs = None
-        trunk_inputs = None
-        #return super().block_eval(x)
+        """
+        TODO param types and descriptions
+        """
+        (branch_inputs, trunk_inputs) = torch.split(x, x.shape[1] - 1, dim=1)
         branch_output = self.branch_net(self.transpose_branch_inputs(branch_inputs))
         trunk_output = self.trunk_net(trunk_inputs)
         result = torch.matmul(branch_output, trunk_output.T) + self.bias
         return result
+
 
 
 

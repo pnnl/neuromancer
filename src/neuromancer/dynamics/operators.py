3b66c3c17f14adbe725110ce97dd6297b1074f43
Colby Ham <colby.ham@pnnl.gov>
2024-06-28 15:58:54 -0700

    Finish initial Class documentation for DeepONet

--- src/neuromancer/dynamics/operators.py
+++ src/neuromancer/dynamics/operators.py
@@ -1,13 +1,15 @@
-from typing import TypeVar
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, TypeVar
 
 import torch
 from torch import nn
 
-from neuromancer.modules.blocks import Block
+if TYPE_CHECKING:
+    from neuromancer.modules.blocks import Block
 
 TDeepONet = TypeVar("TDeepONet", bound="DeepONet")
 
-# TODO(Colby): abstract class to inherit from?
 
 class DeepONet(nn.Module):
     """Deep Operator Network."""
@@ -16,35 +18,43 @@ class DeepONet(nn.Module):
             self: TDeepONet,
             branch_net: Block,
             trunk_net: Block,
-            bias=True,
+            bias: bool = True,
     ) -> None:
-        """: param insize_branch: (int) dimensionality of branch net input
-        : param insize_trunk: (int) dimensionality of trunk net input
-        : param widthsize: (int) horizontal size for branch and trunk net
-        : param interactsize: (int) dimensionality of branch net and trunk net output
-        : param depth_branch: (int) depth of branch net
-        : param depth_trunk: (int) depth of trunk net
-        : nonlin: (callable) nonlinear activation function for branch net and trunk net
-        : bias: (bool) Whether to use bias
+        """Deep Operator Network.
+
+        :param branch_net: (Block) Branch network
+        :param trunk_net: (Block) Trunk network
+        :param bias: (bool) Whether to use bias or not
         """
         super().__init__()
         self.branch_net = branch_net
         self.trunk_net = trunk_net
         self.bias = nn.Parameter(torch.zeros(1), requires_grad=not bias)
 
-    def transpose_branch_inputs(self, branch_inputs):
+    @staticmethod
+    def transpose_branch_inputs(branch_inputs: torch.Tensor) -> torch.Tensor:
+        """Transpose branch inputs.
+
+        :param branch_inputs: (torch.Tensor, shape=[Nu, Nsamples])
+        :return: (torch.Tensor, shape=[Nsamples, Nu])
+        """
         transposed_branch_inputs = torch.transpose(branch_inputs, 0, 1)
         return transposed_branch_inputs
 
+    def forward(self: TDeepONet, branch_inputs: torch.Tensor,
+                trunk_inputs: torch.Tensor) -> tuple[
+        torch.Tensor, torch.Tensor, torch.Tensor]:
+        """Forward propagation.
 
-    def reg_error(self: TDeepONet):
-        return self.branch_net.reg_error() + self.trunk_net.reg_error()
-
-    def forward(self, branch_inputs: torch.Tensor, trunk_inputs: torch.Tensor) -> torch.Tensor:
-        """TODO param types and descriptions
+        :param branch_inputs: (torch.Tensor, shape=[Nu, Nsamples])
+        :param trunk_inputs: (torch.Tensor, shape=[Nu, in_size_trunk])
+        :return:
+            output: (torch.Tensor, shape=[Nsamples, Nu]),
+            branch_output: (torch.Tensor, shape=[Nsamples, interact_size]),
+            trunk_output: (torch.Tensor, shape=[Nu, interact_size])
         """
         branch_output = self.branch_net(self.transpose_branch_inputs(branch_inputs))
         trunk_output = self.trunk_net(trunk_inputs)
-        result = torch.matmul(branch_output, trunk_output.T) + self.bias
-        # TODO, return branch_output and trunk_output for control
-        return result
+        output = torch.matmul(branch_output, trunk_output.T) + self.bias
+        # return branch_output and trunk_output as well for control use cases
+        return output, branch_output, trunk_output

f4c9720ddae5fc71db9ef8943cbe2aa3efb65080
EthanKing-pnnl <ethan.king@pnnl.gov>
2024-07-02 16:46:25 -0700

    added lopo module
    added functionality from the this repo: https://github.com/pnnl/LOPO

--- src/neuromancer/modules/__init__.py
+++ src/neuromancer/modules/__init__.py
@@ -2,3 +2,4 @@ from neuromancer.modules import activations
 from neuromancer.modules import blocks
 from neuromancer.modules import functions
 from neuromancer.modules import solvers
+from neuromancer.modules import lopo

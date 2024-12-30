49f0fc0b939f0ac2b9702b78028368c8e6566c2b
Raab, Reilly P <reilly.raab@pnnl.gov>
2024-12-30 09:00:39 -0500

    remove timeout; use mutationObserver

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -1,11 +1,39 @@
+/* After much searching and experimenting, Sphinx's Read-The-Docs theme
+ * Doesn't let you configure the table of contents in the sidebar (neither
+ * {{ toc }} nor {{ toctree }}) to expand the sections that are initially
+ * collaped but show a "+" button icon for expansion.
+ *
+ * Section expansion is handled client-side by javascript, and when
+ * any toc node is clicked, events managed by minified js implement the
+ * collapse/expand/focus logic.
+ *
+ * We therefore need to interpose on this logic with javascript.
+ */
+
 $(document).ready(() => {
 
-  const scriptElement = document.createElement('script');
+  const menu = document.querySelector(".wy-menu ul li:first-child")
+  const localtoc = document.querySelector(".local-toc ul li")
 
-  // Set the source of the script
-  scriptElement.src = '/_static/js/load_late.js';
+  const config = { attributes: true, attributeFilter: ["class"] };
 
-  // Append the script element to the end of the body
-  document.body.appendChild(scriptElement);
+  const observer = new MutationObserver((mutationsList) => {
+    mutationsList.forEach((mutation) => {
+      if (!mutation.target.classList.contains("current")) {
+        mutation.target.classList.add("current");
+      }
+    });
+  });
+
+
+  if (menu) {
+    menu.classList.add("current");
+    observer.observe(menu, config);
+  }
+
+  if (localtoc) {
+    localtoc.classList.add("current");
+    observer.observe(localtoc, config);
+  }
 
 });
